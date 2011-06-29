60343dd96fd18a000fb3e5c7619af5ae3f8ba3e8
Matt Hanna <hanna@broadinstitute.org>
2011-06-29 14:43:17 -0400

    Initial phase of PreQC database generation, plus refactoring of pm input parser and per sample metrics generator to provide supporting methods for PreQC database generation.

--- private/python/generate_per_sample_metrics.py
+++ private/python/generate_per_sample_metrics.py
@@ -4,7 +4,7 @@
 # To run:
 #   /humgen/gsa-hpprojects/software/bin/jython2.5.2/jython \
 #     -J-classpath $STING_HOME/dist/sam-1.47.869.jar:$STING_HOME/dist/picard-1.47.869.jar:$STING_HOME/dist/picard-private-parts-1941.jar \
-#     $STING_HOME/python/generate_per_sample_metrics.py <bam.list> > <output_metrics_file.tsv>
+#     $STING_HOME/private/python/generate_per_sample_metrics.py <bam.list> > <output_metrics_file.tsv>
 #
 # To add a new metric:
 #   - If the metric file is new to Picard, add the relevant parser to the picard-private jar
@@ -50,57 +50,69 @@ def get_sample_summary_metrics(filename):
     file_reader.close()
     return metrics
 
-if len(sys.argv) != 2:
-    print 'USAGE: %s <pipeline_file.yaml>'
-    sys.exit(1)
-if not os.path.exists(sys.argv[1]):
-    print 'BAM list %s not found' % sys.argv[1]
-    sys.exit(1)
-
-bam_list_filename = sys.argv[1]
-
 sample_summary_metrics_types = [ (HsMetrics,'hybrid_selection_metrics'),
                                  (AlignmentSummaryMetrics, 'alignment_summary_metrics'),
                                  (InsertSizeMetrics, 'insert_size_metrics'),
                                  (DbSnpMatchMetrics, 'dbsnp_matches') ]
 
-header = ['sample','FINGERPRINT_LODS','HAPLOTYPES_CONFIDENTLY_MATCHING']
-for metric_type in sample_summary_metrics_types:
-    header.extend(get_sample_summary_metrics_fields(metric_type[0]))
-print string.join(header,'\t')
+def get_full_metrics_fields():
+    headers = ['FINGERPRINT_LODS','HAPLOTYPES_CONFIDENTLY_MATCHING']
+    for metric_type in sample_summary_metrics_types:
+        headers.extend(get_sample_summary_metrics_fields(metric_type[0]))
+    return headers
 
-# get a representative BAM file for each sample, to use as a base path.  Note that this assumes every sample corresponds to the same base path.
-bam_list = open(bam_list_filename,'r')
-samples = dict()
-
-for bam_filename in bam_list:
-    bam_filename = bam_filename.strip()
-    if bam_filename == '':
-        continue
-    bam_filename_tokens = bam_filename.split('/')
-    sample_id = bam_filename_tokens[len(bam_filename_tokens)-3]
-    samples[sample_id] = bam_filename
-bam_list.close()
-
-for sample_id,filename in samples.items():
-    basepath = filename[:filename.rindex('.bam')]
-    
+def get_full_metrics(sample,basepath):
     fingerprinting_summary_metrics = get_all_metrics('%s.%s' % (basepath,'fingerprinting_summary_metrics'))
-
+    
     if fingerprinting_summary_metrics != None:
         haplotypes_confidently_matching = [str(metric.HAPLOTYPES_CONFIDENTLY_MATCHING) for metric in fingerprinting_summary_metrics]
         fingerprint_lods = [str(metric.LOD_EXPECTED_SAMPLE) for metric in fingerprinting_summary_metrics]
     else:
         haplotypes_confidently_matching = []
         fingerprint_lods = []
-
-    data = [sample_id,'c('+string.join(fingerprint_lods,',')+')','c('+string.join(haplotypes_confidently_matching,',')+')']
+        
+    data = ['c('+string.join(fingerprint_lods,',')+')','c('+string.join(haplotypes_confidently_matching,',')+')']
 
     for metrics_type,metrics_extension in sample_summary_metrics_types:
         metrics_pathname = '%s.%s' % (basepath,metrics_extension)
         if os.path.exists(metrics_pathname):
             metrics = get_sample_summary_metrics(metrics_pathname)
-            data.extend([getattr(metrics, metrics_field_name) for metrics_field_name in get_sample_summary_metrics_fields(metrics_type)])
+            data.extend([str(getattr(metrics, metrics_field_name)) for metrics_field_name in get_sample_summary_metrics_fields(metrics_type)])
         else:
             data.extend(['NA' for metrics_field_name in get_sample_summary_metrics_fields(metrics_type)])
-    print string.join(['%s']*len(header),'\t')%tuple(data)
+    return data    
+
+def main():
+    if len(sys.argv) != 2:
+        print 'USAGE: %s <bam files.list>'
+        sys.exit(1)
+    if not os.path.exists(sys.argv[1]):
+        print 'BAM list %s not found' % sys.argv[1]
+        sys.exit(1)
+
+    bam_list_filename = sys.argv[1]
+
+    header = ['sample']
+    for metric_type in sample_summary_metrics_types:
+        header.extend(get_sample_summary_metrics_fields(metric_type[0]))
+    print string.join(header,'\t')
+
+    # get a representative BAM file for each sample, to use as a base path.  Note that this assumes every sample corresponds to the same base path.
+    bam_list = open(bam_list_filename,'r')
+    samples = dict()
+
+    for bam_filename in bam_list:
+        bam_filename = bam_filename.strip()
+        if bam_filename == '':
+            continue
+        bam_filename_tokens = bam_filename.split('/')
+        sample_id = bam_filename_tokens[len(bam_filename_tokens)-3]
+        samples[sample_id] = bam_filename
+    bam_list.close()
+
+    for sample_id,filename in samples.items():
+        basepath = filename[:filename.rindex('.bam')]
+        print string.join([sample_id]+get_full_metrics(sample_id,basepath),'\t')
+
+if __name__ == "__main__":
+    main()
