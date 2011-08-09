09b9d3d6981941e66436f31ce6fb5e84db1c7ef7
Eric Banks <ebanks@broadinstitute.org>
2011-08-09 12:20:50 -0400

    Move reference to .rods away from symlinks in preparation for their going away in GATK 1.2

--- private/java/src/org/broadinstitute/sting/pipeline/ReferenceData.java
+++ private/java/src/org/broadinstitute/sting/pipeline/ReferenceData.java
@@ -61,8 +61,8 @@ public enum ReferenceData {
         } else if ("hg19".equals(name)) {
             this.reference = "/seq/references/Homo_sapiens_assembly19/v1/Homo_sapiens_assembly19.fasta";
             this.refseq = REFSEQ_DIR + "refGene-big-table-hg19.txt";
-            dbsnps.put(129, DBSNP_DIR + "dbsnp_129_b37.vcf");
-            dbsnps.put(132, DBSNP_DIR + "dbsnp_132_b37.vcf");
+            dbsnps.put(129, DBSNP_DIR + "dbsnp_129_b37.leftAligned.vcf");
+            dbsnps.put(132, DBSNP_DIR + "dbsnp_132_b37.leftAligned.vcf");
         } else
             throw new UnsupportedOperationException("Unknown reference: " + name);
         this.dbsnps = Collections.unmodifiableMap(dbsnps);
