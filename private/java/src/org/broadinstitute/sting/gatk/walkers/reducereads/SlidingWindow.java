02d16ce9b249650dfcb36416f582e29b233fd42c
Mauricio Carneiro <carneiro@broadinstitute.org>
2011-09-23 12:35:38 -0400

    Added separate threshold for indels to trigger variant regions
    Insertions and deletions now have a separate threshold to trigger a variant region. The default is 0.01, meaning that every site with at least 1 in a 100 insertion/deletion will be triggered as a variant region.

--- private/java/src/org/broadinstitute/sting/gatk/walkers/reducereads/SlidingWindow.java
+++ private/java/src/org/broadinstitute/sting/gatk/walkers/reducereads/SlidingWindow.java
@@ -5,6 +5,7 @@ import net.sf.samtools.Cigar;
 import net.sf.samtools.CigarElement;
 import net.sf.samtools.SAMFileHeader;
 import net.sf.samtools.SAMRecord;
+import org.broadinstitute.sting.utils.BaseUtils;
 import org.broadinstitute.sting.utils.MathUtils;
 
 import java.util.Iterator;
@@ -38,18 +39,20 @@ public class SlidingWindow {
     private String readName;
 
     // Additional parameters
-    private double MIN_ALT_BASE_PROPORTION_TO_TRIGGER_VARIANT;    // proportion has to be greater than this value
+    private double MIN_ALT_BASE_PROPORTION_TO_TRIGGER_VARIANT;    // proportion has to be greater than this value to trigger variant region due to mismatches
+    private double MIN_INDEL_BASE_PROPORTION_TO_TRIGGER_VARIANT;  // proportion has to be greater than this value to trigger variant region due to deletions
     private int MIN_BASE_QUAL_TO_COUNT;                           // qual has to be greater than or equal to this value
     private int MAX_QUAL_COUNT;                                   // to avoid blowing up the qual field of a consensus site
     private int MIN_MAPPING_QUALITY;
 
 
-    public SlidingWindow(String contig, int contigIndex, int contextSize, SAMFileHeader header, Object readGroupAttribute, int windowNumber, double minAltProportionToTriggerVariant, int minBaseQual, int maxQualCount, int minMappingQuality) {
+    public SlidingWindow(String contig, int contigIndex, int contextSize, SAMFileHeader header, Object readGroupAttribute, int windowNumber, final double minAltProportionToTriggerVariant, final double minIndelProportionToTriggerVariant, int minBaseQual, int maxQualCount, int minMappingQuality) {
         this.startLocation = -1;
         this.stopLocation = -1;
         this.contextSize = contextSize;
 
         this.MIN_ALT_BASE_PROPORTION_TO_TRIGGER_VARIANT = minAltProportionToTriggerVariant;
+        this.MIN_INDEL_BASE_PROPORTION_TO_TRIGGER_VARIANT = minIndelProportionToTriggerVariant;
         this.MAX_QUAL_COUNT = maxQualCount;
         this.MIN_BASE_QUAL_TO_COUNT = minBaseQual;
         this.MIN_MAPPING_QUALITY = minMappingQuality;
@@ -454,14 +457,14 @@ public class SlidingWindow {
         }
 
         private boolean isVariantFromInsertions() {
-            return ((double) insertionsToTheRight / baseCounts.totalCount()) > MIN_ALT_BASE_PROPORTION_TO_TRIGGER_VARIANT;
+            return ((double) insertionsToTheRight / baseCounts.totalCount()) > MIN_INDEL_BASE_PROPORTION_TO_TRIGGER_VARIANT;
         }
 
         /**
          * Deletions are already counted as mismatches but even if the most common base is a deletion, we need to make this a variant site
          */
         private boolean isVariantFromDeletions() {
-            return baseCounts.baseWithMostCounts() == BaseIndex.D.getByte();
+            return baseCounts.baseWithMostCounts() == BaseIndex.D.getByte() || baseCounts.baseCountProportion(BaseIndex.D) > MIN_INDEL_BASE_PROPORTION_TO_TRIGGER_VARIANT;
         }
 
         private boolean isVariantFromMismatches() {
