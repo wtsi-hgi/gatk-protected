ded8c6d6ed91503805700cb1c135dcd1d0ca53c5
Mark DePristo <depristo@broadinstitute.org>
2011-11-09 14:35:50 -0500

    mergeEvals allows you to treat -eval 1.vcf -eval 2.vcf as a single call set
    -- A bit of code cleanup in VCFUtils
    -- VariantEval table to create 1000G Phase I variant summary table
    -- First version of 1000G Phase I summary table Qscript

---
+++ private/scala/qscript/org/broadinstitute/sting/queue/qscripts/analysis/G1KPhaseISummaryTable.scala
@@ -0,0 +1,98 @@
+/*
+ * Copyright (c) 2011, The Broad Institute
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package org.broadinstitute.sting.queue.qscripts.analysis
+
+/*
+ * Copyright (c) 2011, The Broad Institute
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+import org.broadinstitute.sting.queue.extensions.gatk._
+import org.broadinstitute.sting.queue.QScript
+import java.io.FileWriter
+import org.broadinstitute.sting.utils.exceptions.UserException
+import org.broadinstitute.sting.utils.variantcontext.VariantContext
+import org.broadinstitute.sting.gatk.walkers.genotyper.GenotypeLikelihoodsCalculationModel
+
+class G1KPhaseISummaryTable extends QScript {
+  qscript =>
+
+  @Argument(shortName = "L", fullName = "intervals", doc="intervals", required=false)
+  val myIntervals: List[String] = null;
+
+//  val b37_decoy = new File("/humgen/1kg/reference/human_g1k_v37_decoy.fasta")
+  val bundle = new File("/humgen/gsa-hpprojects/GATK/bundle/current/b37/")
+  val b37 = new File(bundle.getPath + "/human_g1k_v37.fasta")
+  val dbSNP_b37 = new File(bundle.getPath + "/dbsnp_132.b37.vcf")
+  val dbSNP_b37_129 = new File(bundle.getPath + "/dbsnp_132.b37.excluding_sites_after_129.vcf")
+
+  val populations = List("EUR", "ASN", "AFR", "AMR", "ALL")
+
+  val callsets = Range(1,22).map(new File("/humgen/1kg/releases/main_project_phaseI/ALL.chr%d.merged_beagle_mach.20101123.snps_indels_svs.genotypes.vcf.gz".format(_)))
+
+  def script = {
+    for ( population <- populations )
+      add(new evalVariants(pop))
+    }
+  }
+
+  trait UNIVERSAL_GATK_ARGS extends CommandLineGATK {
+    logging_level = "INFO";
+    memoryLimit = 2;
+    reference_sequence = b37
+    intervalsString = myIntervals
+  }
+
+  // 5.) Variant Evaluation Base(OPTIONAL)
+  class evalVariants(pop: String) extends VariantEval with UNIVERSAL_GATK_ARGS {
+    for ( callset <- callsets )
+      this.eval :+= callset
+    this.mergeEvals = true
+    this.D = dbSNP_b37_129
+    this.sample = List("%s.samples.list".format(pop))
+    this.out = List("%s.samples.eval".format(pop))
+    this.noEV = true
+    this.EV = List("G1KPhaseITable")
+  }
+}
