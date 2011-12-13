84e4505f35840b83bb5ed49e204684e89865b96d
Ryan Poplin <rpoplin@broadinstitute.org>
2011-12-13 17:41:10 -0500

    Adding temporary read filter to HaplotypeCaller integration test while ReadClipper contracts are being worked out.

--- private/java/test/org/broadinstitute/sting/gatk/walkers/haplotypecaller/HaplotypeCallerIntegrationTest.java
+++ private/java/test/org/broadinstitute/sting/gatk/walkers/haplotypecaller/HaplotypeCallerIntegrationTest.java
@@ -12,15 +12,15 @@ public class HaplotypeCallerIntegrationTest extends WalkerTest {
     final String INTERVALS_FILE = validationDataLocation + "NA12878.HiSeq.b37.chr20.10_11mb.test.intervals";
     final String RECAL_FILE = validationDataLocation + "NA12878.kmer.7.recal_data.csv";
 
-    private void RRTest(String bam, String args, String md5) {
-        String base = String.format("-T HaplotypeCaller -R %s -I %s -L %s -recalFile %s", REF, bam, INTERVALS_FILE, RECAL_FILE) + " -o %s";
-        WalkerTestSpec spec = new WalkerTestSpec(base + args, Arrays.asList(md5));
+    private void HCTest(String bam, String args, String md5) {
+        final String base = String.format("-T HaplotypeCaller -R %s -I %s -L %s -recalFile %s", REF, bam, INTERVALS_FILE, RECAL_FILE) + " -NO_HEADER -o %s";
+        final WalkerTestSpec spec = new WalkerTestSpec(base + " " + args, Arrays.asList(md5));
         executeTest("testHaplotypeCaller: args=" + args, spec);
     }
 
     @Test
     public void testHaplotypeCallerSingleSample() {
-        RRTest(BAM, "", "7b2a527f182bb238798eeca74330dd3c");
+        HCTest(BAM, "-rf UnmappedRead", "5157c024edd60ec342357b13c6baae3d");
     }
 }
 
