a91f9844c98bd5d0e3fd7c8cd7f0fe05109ad6c3
Ryan Poplin <rpoplin@broadinstitute.org>
2012-02-06 11:10:24 -0500

    Updating Covariate interface with Mauricio to include an errorModel parameter. On the fly recalibration of base insertion and base deletion quals is live for the HaplotypeCaller

--- private/java/test/org/broadinstitute/sting/gatk/walkers/haplotypecaller/HaplotypeCallerIntegrationTest.java
+++ private/java/test/org/broadinstitute/sting/gatk/walkers/haplotypecaller/HaplotypeCallerIntegrationTest.java
@@ -21,12 +21,12 @@ public class HaplotypeCallerIntegrationTest extends WalkerTest {
     @Test
     public void testHaplotypeCallerMultiSample() {
 
-        HCTest(CEUTRIO_BAM, "-rf UnmappedRead", "8564c88d5ddcb195c90dc190e61b623e");
+        HCTest(CEUTRIO_BAM, "-rf UnmappedRead", "83db77c35d9b06583213f0e40e3bcaf3");
     }
 
     @Test
     public void testHaplotypeCallerSingleSample() {
-        HCTest(NA12878_BAM, "-rf UnmappedRead", "3342899653efe68e4f85a014e3f15caa");
+        HCTest(NA12878_BAM, "-rf UnmappedRead", "725e4a0535df3533de05e76bcb2cb29d");
     }
 }
 
