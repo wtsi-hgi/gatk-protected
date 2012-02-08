d1a0e3e6ce0dc1429c2b12ead8b45d6f1a7c2da5
Ryan Poplin <rpoplin@broadinstitute.org>
2012-02-08 16:26:32 -0500

    Incorporating feedback from Mauricio on the plots

--- private/R/AnalyzeCovariates.R
+++ private/R/AnalyzeCovariates.R
@@ -29,7 +29,8 @@ for(cov in levels(data$Covariate)) { # for each covariate in turn
   print(p + geom_point(aes(color=Recalibration)) + scale_color_manual(values=c("maroon1","blue")) +
     opts(title = paste("Base recalibration accuracy", cov, "All read groups", sep=" - ")))
   print(p + geom_point(aes(color=Recalibration)) + scale_color_manual(values=c("maroon1","blue")) + facet_wrap(~ ReadGroup) +
-    opts(title = paste("Base recalibration accuracy", cov, "Per read group", sep=" - ")))
+    opts(title = paste("Base recalibration accuracy", cov, "Per read group", sep=" - ")) +
+    opts(axis.text.x=theme_text(angle=70, hjust=0)))
   
   # plot Reported quality versus Empirical quality as a function of the covariate value
   orig = subset(d, Recalibration == "Original")
@@ -52,12 +53,13 @@ for(cov in levels(data$Covariate)) { # for each covariate in turn
   if( cov != "QualityScore" ) {
     p <- ggplot(d, aes(x=Value,y=Qreported,size=nMismatches)) +
       xlab(paste(cov,"Covariate")) +
-      ylab("Mean Reported Base Quality") +
+      ylab("Mean Base Quality Score") +
       blankTheme
     print(p + geom_point(aes(color=Recalibration)) + scale_color_manual(values=c("maroon1","blue")) +
-      opts(title = paste("Mean reported base quality", cov, "All read groups", sep=" - ")))
+      opts(title = paste("Mean base quality score", cov, "All read groups", sep=" - ")))
     print(p + geom_point(aes(color=Recalibration)) + scale_color_manual(values=c("maroon1","blue")) + facet_wrap(~ ReadGroup) +
-      opts(title = paste("Mean reported base quality", cov, "Per read group", sep=" - ")))
+      opts(title = paste("Mean base quality score", cov, "Per read group", sep=" - ")) +
+      opts(axis.text.x=theme_text(angle=70, hjust=0)))
   }
 
   # plot a histogram of the number of observations of the covariate value
@@ -65,12 +67,11 @@ for(cov in levels(data$Covariate)) { # for each covariate in turn
     xlab(paste(cov,"Covariate")) +
     ylab("Number of Observations") +
     blankTheme
-  if( cov == "QualityScore" ) {
-    print(p + geom_histogram(aes(fill=Recalibration,weight=nBases),alpha=0.6,binwidth=1,position="identity") + scale_fill_manual(values=c("maroon1","blue")) +      
-      scale_y_continuous(formatter="comma") + opts(title = paste("Shift in base quality scores", "All read groups", sep=" - ")))
-  }
+  print(p + geom_histogram(aes(fill=Recalibration,weight=nBases),alpha=0.6,binwidth=1,position="identity") + scale_fill_manual(values=c("maroon1","blue")) +      
+    scale_y_continuous(formatter="comma") + opts(title = paste("Histogram of observations", cov, "All read groups", sep=" - ")))
   print(p + geom_histogram(aes(fill=Recalibration,weight=nBases),position="identity",alpha=0.6,binwidth=1) + 
     scale_fill_manual(values=c("maroon1","blue")) + facet_wrap(~ ReadGroup) + scale_y_continuous(formatter="comma") +
-    opts(title = paste("Histogram of observations", cov, "Per read group", sep=" - ")))
+    opts(title = paste("Histogram of observations", cov, "Per read group", sep=" - ")) +
+    opts(axis.text.x=theme_text(angle=70, hjust=0)))
 }
 dev.off()
