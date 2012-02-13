77fa893d01ab87a73a7ea9c6ba16ee3692ca6952
Ryan Poplin <rpoplin@broadinstitute.org>
2012-02-13 15:44:30 -0500

    Updating BQSR scala script to take any number of known sites files and to use the scatter count input argument.

--- private/scala/qscript/org/broadinstitute/sting/queue/qscripts/bqsr/BQSR.scala
+++ private/scala/qscript/org/broadinstitute/sting/queue/qscripts/bqsr/BQSR.scala
@@ -7,18 +7,18 @@ class BQSR extends QScript {
   @Argument(shortName = "o", required=true,  doc = "output file name")   var output: File = _
   @Argument(shortName = "i", required=false, doc = "Intervals file")     var intervalsFile: List[File] = Nil
   @Argument(shortName = "r", required=false, doc = "Reference sequence") var referenceFile: File = new File("/humgen/1kg/reference/human_g1k_v37_decoy.fasta")
-  @Argument(shortName = "d", required=false, doc = "dbSNP sequence")     var dbSNPFile: File = new File("/humgen/gsa-hpprojects/GATK/data/Comparisons/Validated/dbSNP/dbsnp_129_b37.leftAligned.vcf")
-  @Argument(shortName = "s", required=false, doc = "scatter/gather")     var scatterCount: Int = 100
+  @Argument(shortName = "s", required=false, doc = "scatter/gather")     var scatterCount: Int = 200
+  @Input(doc="dbsnp ROD to use (must be in VCF format)", fullName="dbsnp", shortName="d", required=true) var dbSNP: Seq[File] = Seq()
 
   def script {
     val walker = new BaseQualityScoreRecalibration();
     walker.reference_sequence = referenceFile
     walker.intervalsString = intervalsFile
     walker.out = output
-    walker.knownSites :+= new TaggedFile(dbSNPFile, "dbSNP")
+    walker.knownSites ++= dbSNP
     walker.input_file = bamList
     walker.memoryLimit = 8
-    walker.scatterCount = 50
+    walker.scatterCount = scatterCount
     add(walker)
   }
 }
