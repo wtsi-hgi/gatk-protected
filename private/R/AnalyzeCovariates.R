0e4187523468be00cc3ab92a79151441f941e296
Mauricio Carneiro <carneiro@broadinstitute.org>
2012-04-06 17:07:57 -0400

    Updated analyze covariates R script for new GATKReport format

--- private/R/AnalyzeCovariates.R
+++ private/R/AnalyzeCovariates.R
@@ -1,19 +1,25 @@
-library("ggplot2")
+## Assumes the data is organized like this:
+#Recalibration  ReadGroup	Covariate	Value	QualityScore	EmpiricalQuality	Errors	Observations
+#Original	    B00EG.4	    Cycle	    2	    2.000	        11.825	            9011003	137161904
+#Original	    B00EG.4	    Cycle	    4	    4.000	        5.001	            526054	1663850
+#Original	    B00EG.4	    Cycle	    5	    5.000	        5.817	            522185	1993294
+#Original	    B00EG.4	    Context	    TGCCGT	6.000	        6.725	            937794	4411346
+#Original	    B00EG.4	    Context	    ACGTTC	7.000	        7.476	            1028074	5749046
+#Original	    B00EG.4	    Cycle	    8	    8.000	        8.006	            883264	5580387
 
-# Assumes the data is organized like this:
-#Recalibration  ReadGroup	Covariate	Value	Qreported	Qempirical	nMismatches	nBases
-#Original	B00EG.4	QualityScore	2	2.000	11.825	9011003	137161904
-#Original	B00EG.4	QualityScore	4	4.000	5.001	526054	1663850
-#Original	B00EG.4	QualityScore	5	5.000	5.817	522185	1993294
-#Original	B00EG.4	QualityScore	6	6.000	6.725	937794	4411346
-#Original	B00EG.4	QualityScore	7	7.000	7.476	1028074	5749046
-#Original	B00EG.4	QualityScore	8	8.000	8.006	883264	5580387
-#Original	B00EG.4	QualityScore	9	9.000	9.045	924957	7422920
-#Original	B00EG.4	QualityScore	10	10.000	9.784	1044520	9938322
-#Original	B00EG.4	QualityScore	11	11.000	9.310	1487921	12694829
+library(ggplot2);
+library(gsalib);
 
-data = read.table("NA12892.recal.dat", head=T)
-data$Accuracy = data$Qempirical - data$Qreported # calculate the delta in accuracy up front as a short hand
+args <- commandArgs(TRUE)
+before = gsa.read.gatkreport(args[1])
+after = gsa.read.gatkreport(args[2])
+a = cbind("Original", before$RecalTable2)
+b = cbind("Recalibrated", after$RecalTable2)
+names(a) = c("Recalibration", names(before$RecalTable2))
+names(b) = c("Recalibration", names(after$RecalTable2))
+data = rbind(a,b)
+
+data$Accuracy = data$EmpiricalQuality - data$QualityScore # calculate the delta in accuracy up front as a short hand
 numRG = length(unique(data$ReadGroup))
 blankTheme = opts(
     panel.grid.major = theme_blank(),
@@ -22,35 +28,25 @@ blankTheme = opts(
     axis.ticks = theme_blank()
   )
 
-pdf("NA12892.recal.pdf",height=7,width=11)
-for(cov in levels(data$Covariate)) { # for each covariate in turn  
-  d = data[data$Covariate==cov,] # pull out just the data for this covariate so we can treat the non-numeric values appropriately
-  if( cov == "Dinuc" ) {
-    d$Value = as.character(d$Value)
-  } else {
-    d$Value = as.numeric(levels(d$Value))[as.integer(d$Value)] # efficient way to convert factors back to their real values
-    if( cov == "QualityScore" ) {
-      d = subset(d,Value > 5) # cutting off low qual bases here, but eventually this will be done in the BQSRv2 code
-    }
-  }
+pdf(args[3],height=7,width=11)
+for(cov in levels(data$CovariateName)) { # for each covariate in turn
+  d = data[data$CovariateName==cov,] # pull out just the data for this covariate so we can treat the non-numeric values appropriately
+  if (cov == "Context") 
+    d$Value = as.character(d$CovariateValue)
+  if (cov == "Cycle")
+    d$Value = as.numeric(levels(d$CovariateValue))[as.integer(d$CovariateValue)] # efficient way to convert factors back to their real values
+
   # plot Accuracy by the covariate value
-  p <- ggplot(d, aes(x=Value,y=Accuracy,size=nMismatches,alpha=nBases)) +
-    geom_abline(intercept=0, slope=0, linetype=2) + 
-    xlab(paste(cov,"Covariate")) +
-    ylab("Base Quality Score Accuracy (Empirical - Reported)") +
-    blankTheme
-  print(p + geom_point(aes(color=Recalibration)) + scale_color_manual(values=c("maroon1","blue")) +
-    opts(title = paste("Base recalibration accuracy", cov, "All read groups", sep=" - ")))
-  print(p + geom_point(aes(color=Recalibration)) + scale_color_manual(values=c("maroon1","blue")) + facet_wrap(~ ReadGroup) +
-    opts(title = paste("Base recalibration accuracy", cov, "Per read group", sep=" - ")) +
-    opts(axis.text.x=theme_text(angle=70, hjust=0)))
-  
+  p <- qplot(data=d, x=Value, y=Accuracy, size=Errors, alpha=Observations, color=Recalibration, xlab = paste(cov,"Covariate"), ylab = "Base Quality Score Accuracy (Empirical - Reported)") + geom_abline(intercept=0, slope=0, linetype=2) + scale_color_manual(values=c("maroon1","blue")) + blankTheme
+  print(p + opts(title = paste("Base recalibration accuracy", cov, "All read groups", sep=" - ")))
+  print(p + facet_wrap(~ ReadGroup) + opts(title = paste("Base recalibration accuracy", cov, "Per read group", sep=" - "), axis.text.x=theme_text(angle=70, hjust=0)))
+
   # plot Reported quality versus Empirical quality as a function of the covariate value
   orig = subset(d, Recalibration == "Original")
-  rmseOriginal = sqrt(sum((orig$Qreported-orig$Qempirical)^2 * orig$nBases) / sum(orig$nBases))
+  rmseOriginal = sqrt(sum((orig$QualityScore-orig$EmpiricalQuality)^2 * orig$Observations) / sum(orig$Observations))
   recal = subset(d, Recalibration == "Recalibrated")
-  rmseRecalibrated = sqrt(sum((recal$Qreported-recal$Qempirical)^2 * recal$nBases) / sum(recal$nBases))   
-  p <- ggplot(d, aes(x=Qreported,y=Qempirical,size=nMismatches,alpha=nBases)) +
+  rmseRecalibrated = sqrt(sum((recal$QualityScore-recal$EmpiricalQuality)^2 * recal$Observations) / sum(recal$Observations))   
+  p <- ggplot(d, aes(x=QualityScore,y=EmpiricalQuality,size=Errors,alpha=Observations)) +
     geom_abline(intercept=0, slope=1, linetype=2) + 
     xlab("Reported Base Quality") +
     ylab("Empirical Base Quality") +
@@ -63,28 +59,26 @@ for(cov in levels(data$Covariate)) { # for each covariate in turn
     opts(title = paste("Base recalibration curve", cov, "Per read group", sep=" - ")))
   
   # plot mean Reported quality as a function of the covariate value
-  if( cov != "QualityScore" ) {
-    p <- ggplot(d, aes(x=Value,y=Qreported,size=nMismatches,alpha=nBases)) +
-      xlab(paste(cov,"Covariate")) +
-      ylab("Mean Base Quality Score") +
-      blankTheme
-    print(p + geom_point(aes(color=Recalibration)) + scale_color_manual(values=c("maroon1","blue")) +
-      opts(title = paste("Mean base quality score", cov, "All read groups", sep=" - ")))
-    print(p + geom_point(aes(color=Recalibration)) + scale_color_manual(values=c("maroon1","blue")) + facet_wrap(~ ReadGroup) +
-      opts(title = paste("Mean base quality score", cov, "Per read group", sep=" - ")) +
-      opts(axis.text.x=theme_text(angle=70, hjust=0)))
-  }
+  p <- ggplot(d, aes(x=Value,y=QualityScore,size=Errors,alpha=Observations)) +
+    xlab(paste(cov,"Covariate")) +
+    ylab("Mean Base Quality Score") +
+    blankTheme
+  print(p + geom_point(aes(color=Recalibration)) + scale_color_manual(values=c("maroon1","blue")) +
+    opts(title = paste("Mean base quality score", cov, "All read groups", sep=" - ")))
+  print(p + geom_point(aes(color=Recalibration)) + scale_color_manual(values=c("maroon1","blue")) + facet_wrap(~ ReadGroup) +
+    opts(title = paste("Mean base quality score", cov, "Per read group", sep=" - ")) +
+    opts(axis.text.x=theme_text(angle=70, hjust=0)))
+
 
   # plot a histogram of the number of observations of the covariate value
   p <- ggplot(d, aes(x=Value)) +
     xlab(paste(cov,"Covariate")) +
     ylab("Number of Observations") +
     blankTheme
-  print(p + geom_histogram(aes(fill=Recalibration,weight=nBases),alpha=0.6,binwidth=1,position="identity") + scale_fill_manual(values=c("maroon1","blue")) +      
+  print(p + geom_histogram(aes(fill=Recalibration,weight=Observations),alpha=0.6,binwidth=1,position="identity") + scale_fill_manual(values=c("maroon1","blue")) +      
     scale_y_continuous(formatter="comma") + opts(title = paste("Histogram of observations", cov, "All read groups", sep=" - ")))
-  print(p + geom_histogram(aes(fill=Recalibration,weight=nBases),position="identity",alpha=0.6,binwidth=1) + 
+  print(p + geom_histogram(aes(fill=Recalibration,weight=Observations),position="identity",alpha=0.6,binwidth=1) + 
     scale_fill_manual(values=c("maroon1","blue")) + facet_wrap(~ ReadGroup) + scale_y_continuous(formatter="comma") +
     opts(title = paste("Histogram of observations", cov, "Per read group", sep=" - ")) +
     opts(axis.text.x=theme_text(angle=70, hjust=0)))
 }
-dev.off()
