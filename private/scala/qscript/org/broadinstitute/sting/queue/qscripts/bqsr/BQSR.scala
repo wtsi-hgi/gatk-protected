948718f4628a075a0701c3e74d149e2e06d3467d
Mauricio Carneiro <carneiro@broadinstitute.org>
2012-04-23 14:43:42 -0400

    BQSR triage #4
       * fixed queue script plot file names
       * updated the ReadGroupCovariate to use the platform unit instead of sample + lane.
       * fixed plotting of marginalized reported qualities

--- private/scala/qscript/org/broadinstitute/sting/queue/qscripts/bqsr/BQSR.scala
+++ private/scala/qscript/org/broadinstitute/sting/queue/qscripts/bqsr/BQSR.scala
@@ -10,18 +10,24 @@ class BQSR extends QScript {
 
   @Argument(shortName = "b",  required=true,  doc = "List of BAM files")        var bamList: File = _
   @Argument(shortName = "i",  required=false, doc = "Intervals file")           var intervalsFile: List[File] = Nil
+  @Argument(shortName = "mcs",required=false, doc = "mismatches context size")  var mismatchesContextSize = 3
+  @Argument(shortName = "ics",required=false, doc = "insertions context size")  var insertionsContextSize = 3
+  @Argument(shortName = "dcs",required=false, doc = "deletions context size")   var deletionsContextSize = 3
   @Argument(shortName = "r",  required=false, doc = "Reference sequence")       var referenceFile: File = new File("/humgen/1kg/reference/human_g1k_v37_decoy.fasta")
   @Argument(shortName = "m",  required=false, doc = "memory limit")             var memLimit: Int = 4
   @Argument(shortName = "qq", required=false, doc = "quantization lvls")        var nLevels: Int = 0
   @Argument(shortName = "k",  required=false, doc = "keep intermediate files")  var keepIntermediates: Boolean = true
   @Argument(shortName = "s",  required=false, doc = "scatter/gather")           var scatterCount: Int = 50
+  @Argument(shortName = "np",  required=false, doc = "don't generate plots")    var noPlots: Boolean = false
+
 
 
 
   def script() {
-    val bams = QScriptUtils.createSeqFromFile(bamList);
+    val bams = QScriptUtils.createSeqFromFile(bamList)
+    val recalibrate: Boolean = recalFile == null
     for (bam <- bams) {
-      if (recalFile == null) {
+      if (recalibrate) {
         recalFile = swapExt(bam, ".bam", ".original.grp")
         val bqsr = new BaseQualityScoreRecalibrator();
         bqsr.reference_sequence = referenceFile
@@ -31,27 +37,33 @@ class BQSR extends QScript {
         bqsr.input_file :+= bam
         bqsr.memoryLimit = memLimit
         bqsr.qq = nLevels
+        bqsr.mcs = mismatchesContextSize
+        bqsr.ics = insertionsContextSize
+        bqsr.dcs = deletionsContextSize
         bqsr.no_plots = true
         bqsr.solid_nocall_strategy = RecalDataManager.SOLID_NOCALL_STRATEGY.PURGE_READ
         bqsr.scatterCount = scatterCount
         add(bqsr)
       }
 
-      val recal = new BaseQualityScoreRecalibrator();
-      recal.reference_sequence = referenceFile
-      recal.intervalsString = intervalsFile
-      recal.out = swapExt(bam, ".bam", ".recal.grp")
-      recal.knownSites ++= dbSNP
-      recal.input_file :+= bam
-      recal.memoryLimit = memLimit
-      recal.qq = nLevels
-      recal.solid_nocall_strategy = RecalDataManager.SOLID_NOCALL_STRATEGY.PURGE_READ
-      recal.scatterCount = scatterCount
-      recal.keep_intermediate_files = keepIntermediates
-      recal.BQSR = recalFile
-      add(recal)
-
-      //todo -- add routine that generates plot given the two grp files
+      if (!noPlots) {
+        val recal = new BaseQualityScoreRecalibrator();
+        recal.reference_sequence = referenceFile
+        recal.intervalsString = intervalsFile
+        recal.out = swapExt(bam, ".bam", ".recal.grp")
+        recal.knownSites ++= dbSNP
+        recal.input_file :+= bam
+        recal.memoryLimit = memLimit
+        recal.qq = nLevels
+        recal.mcs = mismatchesContextSize
+        recal.ics = insertionsContextSize
+        recal.dcs = deletionsContextSize
+        recal.solid_nocall_strategy = RecalDataManager.SOLID_NOCALL_STRATEGY.PURGE_READ
+        recal.scatterCount = scatterCount
+        recal.keep_intermediate_files = keepIntermediates
+        recal.BQSR = recalFile
+        add(recal)
+      }
     }
   }
 }
