7290bcc65546fc8ca3407213412c353dd4f05e03
Mark DePristo <depristo@broadinstitute.org>
2012-06-06 14:02:25 -0400

    Convenient python script to update md5s in one go across all files
    -- integration tests now write out mismatches file, which this script reads in, walks the file system to find files with these md5s, and updates them in the files.
    -- Should be run like: python private/python/updateMD5s.py integrationtests/md5mismatches.txt public/java/test
    -- Note that before using this code you should really commit your code for safety
    -- Note also that you ***MUST*** review the differences before running this code.  It's not ok to just update md5s willy-nilly
    -- Doc from the code:
    
        Update the MD5s across multiple files.
    
        The first argument is the database of MD5 updates produced automatically by the
        integrationtests, which lives in integrationtest/md5mismatches.txt.
    
        The second (and more) argument(s) is the path to the source directory containing the java / scala
        code with md5s to update.  This program crawls that directory looking for .java
        and .scala files to update, id's those with MD5s in the md5mismatches.txt files and
        updates these files.
    
        It is highly recommended that you commit your code before updating the MD5s to ensure
        that if things go wrong with this script that you don't lose any work.

---
+++ private/python/updateMD5s.py
@@ -0,0 +1,130 @@
+from optparse import OptionParser
+import itertools
+import os
+
+def main():
+    global OPTIONS
+    usage = """usage: %prog [options] mode md5mismatches ... sourceDir
+    Update the MD5s across multiple files.
+    
+    The first argument is the database of MD5 updates produced automatically by the
+    integrationtests, which lives in integrationtest/md5mismatches.txt.
+    
+    The second (and more) argument(s) is the path to the source directory containing the java / scala
+    code with md5s to update.  This program crawls that directory looking for .java
+    and .scala files to update, id's those with MD5s in the md5mismatches.txt files and
+    updates these files.
+    
+    It is highly recommended that you commit your code before updating the MD5s to ensure
+    that if things go wrong with this script that you don't lose any work."""
+    
+    parser = OptionParser(usage=usage)
+    
+    parser.add_option("-v", "--verbose", dest="verbose",
+                        action='store_true', default=False,
+                        help="If provided, verbose progress will be enabled")         
+
+    parser.add_option("-m", "--allowMissingMismatches", dest="allowMissingMismatches",
+                        action='store_true', default=False,
+                        help="If provided, we will tolerate not having a source file to update for every mismatch")         
+
+    parser.add_option("-n", "--dry", dest="dry",
+                        action='store_true', default=False,
+                        help="If provided, don't actually update anything")         
+
+    (OPTIONS, args) = parser.parse_args()
+    if len(args) != 2:
+        parser.error("Requires a md5mismatch db and at least one path to find files to update")
+        
+    print 'Reading md5 mismatches from', args[0], '...' 
+    md5DB = readMD5Mismatches(args[0])
+    print '    Read', len(md5DB), 'mismatches' 
+
+    print '\nLooking for files to update...'
+    filesToUpdate = []
+    for file in findFilesInDirectories(args[1:]):
+        found = findMismatchSources(md5DB, file)
+        if found:
+            filesToUpdate.append(found)
+            
+    print '\nEnumerating files we will update...'
+    for mismatch in md5DB:
+        if mismatch.source == None and not OPTIONS.allowMissingMismatches:
+            raise Exception("Didn't find a source file to update for mismatch %s" % str(mismatch))
+        print '    Mismatch %s will update file %s' % (mismatch.oldMD5, mismatch.source)
+
+    print '\nUpdating files...'
+    data = sorted(md5DB, key=Mismatch.getSource)
+    for fileToUpdate, md5s in itertools.groupby(md5DB, Mismatch.getSource):
+        print '    Updating file', fileToUpdate
+        md5s = list(md5s)
+        for md5 in md5s: print '        with %s => %s' % (md5.oldMD5, md5.newMD5)
+        updateFile(md5s, fileToUpdate)
+
+class Mismatch:
+    def __init__(self, oldMD5, newMD5, name):
+        self.oldMD5 = oldMD5
+        self.newMD5 = newMD5
+        self.name = name
+        self.source = None
+        
+    def setSource(self, file):
+        if self.source != None:
+            raise Exception("Multiple source files to update for test %s.  Found in %s and %s" % (str(self), self.source, file))
+        self.source = file
+
+    def getSource(self):
+        return self.source
+        
+    def __str__(self):
+        return "[Mismatch md5=%s name=%s]" % (self.oldMD5, self.name) 
+        
+def readMD5Mismatches(file):
+    def parseOne(line):
+        parts = line.strip().split("\t")
+        if len(parts) != 3:
+            raise Exception("Unexpected number of elements in md5 file at line: " + line)
+        return Mismatch(parts[0], parts[1], parts[2]) 
+        
+    return [parseOne(line) for line in open(file) if not line.startswith("expected")]
+    
+def findMismatchSources(db, file):
+    txt = open(file).read()
+    found = False
+    for mismatch in db:
+        if txt.find(mismatch.oldMD5) != -1:
+            mismatch.setSource(file)
+            print '    MD5 update found: expected=%s => new=%s in file %s' % ( mismatch.oldMD5, mismatch.newMD5, file )
+            found = True
+    return False
+    
+def findFilesInDirectories(directoriesOrFiles):
+    if len(directoriesOrFiles) == 0:
+        raise Exception("BUG: no directories provided")
+
+    filesToConsiderUpdating = []
+    for top in directoriesOrFiles:
+        for root, dirs, files in os.walk(top):
+            for name in files:
+                if name.endswith(".java") or name.endswith(".scala"):
+                    filesToConsiderUpdating.append(os.path.join(root, name))
+    return filesToConsiderUpdating
+
+def updateFile(db, file):
+    if len(db) == 0:
+        raise Exception("BUG: DB has no mismatches!")
+
+    fd = open(file)
+    txt = fd.read()
+    for mismatch in db:
+        txt = txt.replace(mismatch.oldMD5, mismatch.newMD5)
+    fd.close()
+    print '        => Updated text'
+    if not OPTIONS.dry:
+        fd = open(file, "w")
+        fd.write(txt)
+        fd.close()
+        print '      => Updated file on disk'
+
+if __name__ == "__main__":
+    main()
