0b615028e6c22052e851b15ce38fbc17972f1b32
Eric Banks <ebanks@broadinstitute.org>
2012-06-11 15:44:55 -0400

    Re-enabling BQSR integration tests

--- private/java/test/org/broadinstitute/sting/gatk/walkers/bqsr/BQSRIntegrationTest.java
+++ private/java/test/org/broadinstitute/sting/gatk/walkers/bqsr/BQSRIntegrationTest.java
@@ -10,23 +10,23 @@ import java.util.Arrays;
  * @since 3/27/12
  */
 public class BQSRIntegrationTest extends WalkerTest {
-    @Test(enabled = false)
+    @Test(enabled = true)
     public void recalibrateTest() {
         String REF = "public/testdata/exampleFASTA.fasta";
         String BAM = "public/testdata/exampleBAM.bam";
         String DBSNP = "public/testdata/exampleDBSNP.vcf";
         String base = String.format("-T BaseQualityScoreRecalibrator -R %s -I %s -knownSites %s", REF, BAM, DBSNP) + " -o %s ";
-        WalkerTestSpec spec = new WalkerTestSpec(base, Arrays.asList("d41d8cd98f00b204e9800998ecf8427e"));
+        WalkerTestSpec spec = new WalkerTestSpec(base, Arrays.asList("71709bb82766f43f367ae9bf09f28e02"));
         executeTest("recalibrateTest", spec);
     }
 
-    @Test(enabled = false)
+    @Test(enabled = true)
     public void onTheFlyRecalibrationTest() {
         String REF = "public/testdata/exampleFASTA.fasta";
         String BAM = "public/testdata/exampleBAM.bam";
         String GRP = "public/testdata/exampleGRP.grp";
         String base = String.format("-T PrintReads -R %s -I %s -BQSR %s", REF, BAM, GRP) + " -o %s ";
-        WalkerTestSpec spec = new WalkerTestSpec(base, Arrays.asList("ba62830dfe91e8bbe1d0fbff90faa5b0"));
+        WalkerTestSpec spec = new WalkerTestSpec(base, Arrays.asList("690c7ab414bd39de72a635aedc1fcd66"));
         executeTest("onTheFlyRecalibrationTest", spec);
     }
 
