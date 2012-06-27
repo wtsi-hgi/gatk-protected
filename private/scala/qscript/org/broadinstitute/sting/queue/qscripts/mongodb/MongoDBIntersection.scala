1f3e9dfa0d839d62f73bf8c97eea5eb85af47cb8
Joel Thibault <thibault@broadinstitute.org>
2012-06-27 14:17:25 -0400

    Copy intervals/intersection processing to new MongoDBIntersection file

--- private/scala/qscript/org/broadinstitute/sting/queue/qscripts/mongodb/MongoDBIntersection.scala
+++ private/scala/qscript/org/broadinstitute/sting/queue/qscripts/mongodb/MongoDBIntersection.scala
@@ -1,21 +1,23 @@
 /**
  * Created with IntelliJ IDEA.
  * User: thibault
- * Date: 5/3/12
- * Time: 4:23 PM
+ * Date: 6/27/12
+ * Time: 3:00 PM
  * To change this template use File | Settings | File Templates.
  */
-
+import org.broadinstitute.sting.gatk.arguments.ValidationExclusion
 import org.broadinstitute.sting.queue.QScript
 import org.broadinstitute.sting.queue.extensions.gatk._
+import org.broadinstitute.sting.utils.interval.IntervalSetRule
 
 /**
  * Tests MongoDB by running SelectVariantsFromMongo in parallel
- * This edition uses scatter/gather to split up the processing
+ * This edition uses intervals and scatter/gather to split up the processing
  */
-class MongoDBTester extends QScript {
+class MongoDBIntersection extends QScript {
   // Create an alias 'qscript' to be able to access variables
   qscript =>
+
 
   // Required arguments.  All initialized to empty values.
 
@@ -28,22 +30,40 @@ class MongoDBTester extends QScript {
   @Input(doc="Samples file.", shortName="sf", required=false)
   var samplesFile: File = _
 
+  @Input(doc="Intervals file.", shortName="L", required=true)
+  var intervalsFile: File = _
+
   @Input(doc="Number of clients.", shortName="c")
   var numClients: Int = _
 
   def script() {
     val selectVariants = new SelectVariantsFromMongo
 
+    selectVariants.unsafe = ValidationExclusion.TYPE.LENIENT_VCF_PROCESSING
     selectVariants.reference_sequence = referenceFile
     selectVariants.variant = vcfFile
 
+    val intersection = true
+
     if (samplesFile != null ) {
       selectVariants.sf :+= samplesFile
-      selectVariants.out = swapExt(qscript.samplesFile, "samples", "%d.vcf".format(numClients))
+      if (intersection) {
+        selectVariants.out = swapExt(qscript.samplesFile, "samples", "%d_intersection.vcf".format(numClients))
+      }
+      else {
+        selectVariants.out = swapExt(qscript.samplesFile, "samples", "%d.vcf".format(numClients))
+      }
     }
     else {
       selectVariants.no_samples = true
       selectVariants.out = "no_samples.vcf"
+    }
+
+    selectVariants.intervals :+= intervalsFile
+
+    if (intersection) {
+      selectVariants.intervals :+= "/home/unix/thibault/scratch/intervals/whole_exome_agilent_1.1_refseq_plus_3_boosters.Homo_sapiens_assembly19.targets.interval_list"
+      selectVariants.isr = IntervalSetRule.INTERSECTION
     }
 
     selectVariants.memoryLimit = 4
