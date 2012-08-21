54da82a618d5f2118afe2a949b4abeb700f1c2c9
Mark DePristo <depristo@broadinstitute.org>
2012-08-21 14:44:11 -0400

    Linear and Quadratic fits for GATKPerformanceOverTime.R

--- private/R/GATKPerformanceOverTime.R
+++ private/R/GATKPerformanceOverTime.R
@@ -1,7 +1,7 @@
 library(gsalib)
 library(ggplot2)
 library(reshape)
-#library(gplots)
+library(splines)
 library(tools)
 
 args <- commandArgs(TRUE)
@@ -19,7 +19,8 @@ if ( onCMDLine ) {
 } else {
   #file <- "/humgen/gsa-hpprojects/dev/depristo/oneOffProjects/parallelBQSR/GATKPerformanceOverTime.jobreport.txt"
   #file <- "/humgen/gsa-hpprojects/dev/depristo/oneOffProjects/parallelCombineVariants2/GATKPerformanceOverTime.jobreport.txt"
-  file <- "~/Desktop/broadLocal/GATK/unstable/GATKPerformanceOverTime.jobreport.txt"
+  file <- "/humgen/gsa-hpprojects/dev/depristo/oneOffProjects/gatkPerformanceOverTime//GATKPerformanceOverTime.jobreport.txt"
+  #file <- "~/Desktop/broadLocal/GATK/unstable/GATKPerformanceOverTime.jobreport.txt"
   #file <- "/humgen/gsa-hpprojects/dev/depristo/oneOffProjects/gatkPerformanceOverTime/Q-24937@gsa1.jobreport.txt"
   outputPDF <- NA
 }
@@ -52,19 +53,26 @@ plotByNSamples <- function(report) {
 }
 
 plotByNT <- function(report, includeFacet = F) {
-  plotCmdByX(report, "nt", includeFacet, logUnit = 2)
+  plotCmdByX(report, "nt", includeFacet, logUnit = 2, T)
 }
 
-plotCmdByX <- function(report, X, includeFacet = F, logUnit = 10) {
+plotCmdByX <- function(report, X, includeFacet = F, logUnit = 10, useWeights=F) {
   report$X <- report[[X]]
+  if ( useWeights )
+    report$weight = 1 / report$X^2
+  else
+    report$weight = 1
   p = ggplot(data=report, aes(x=X, y=runtime, group=gatk, color=gatk))
   if ( includeFacet ) 
     p = p + facet_grid(. ~ assessment, scales="free")
   p = p + geom_jitter()
-  #p = p + geom_point()
-  p = p + geom_smooth()
+
+  # linear and quadratic fits
+  p = p + stat_smooth(method=lm, se=FALSE, aes(weight=weight), linetype="dashed")
+  p = p + geom_smooth(method="lm", se=FALSE, formula = y ~ ns(x,2))
+  
   if ( logUnit == 10 ) {
-    p = p + scale_x_log10(breaks=unique(report$X)) + scale_y_log10()
+    p = p + scale_x_log10() + scale_y_log10()
   } else if ( logUnit == 2 ) {
     p = p + scale_x_continuous(trans = "log2", breaks=unique(report$X)) + scale_y_continuous(trans = "log2")
   }
@@ -73,6 +81,7 @@ plotCmdByX <- function(report, X, includeFacet = F, logUnit = 10) {
   p = p + geom_boxplot(aes(group=interaction(X, gatk)))
   p
 }
+#plotByNT(allReports$CombineVariants.nt)
 
 plotNormalizedByNSamples <- function(report) {
   norm = ddply(report, .(nSamples, assessment), transform, normRuntime = runtime / mean(runtime))
@@ -81,7 +90,7 @@ plotNormalizedByNSamples <- function(report) {
   p = p + geom_boxplot(aes(group=interaction(nSamples, gatk)), outlier.colour="blue")
   #p = p + geom_jitter()
   #p = p + geom_point()
-  p = p + geom_smooth()
+  p = p + geom_smooth(se=FALSE)
   p = p + scale_x_log10()# + scale_y_log10()
   p = p + opts(title=paste("Runtime per nSamples relative to nSamples mean value", report$analysisName))
   print(p)
