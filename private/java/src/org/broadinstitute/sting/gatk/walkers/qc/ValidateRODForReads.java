37cd8d89a956da4a6ec0d4638a5e7ecb932e7efd
Mark DePristo <depristo@broadinstitute.org>
2012-08-30 15:10:58 -0400

    Fix GSA-529: Fix RODs for parallel read walkers
    -- TraverseReadsNano modified to read in all input data before invoking maps, so the input to TraverseReadsNano is a MapData object holding the sam record, the ref context, and the refmetadatatracker.
    -- Update ValidateRODForReads to be tree reducible, using synchronized map and explicitly sort the output map from locations -> counts in onTraversalDone
    -- Expanded integration tests to test nt 1, 2, 4.

--- private/java/src/org/broadinstitute/sting/gatk/walkers/qc/ValidateRODForReads.java
+++ private/java/src/org/broadinstitute/sting/gatk/walkers/qc/ValidateRODForReads.java
@@ -1,25 +1,28 @@
 package org.broadinstitute.sting.gatk.walkers.qc;
 
-import org.broad.tribble.Feature;
 import org.broadinstitute.sting.commandline.Input;
 import org.broadinstitute.sting.commandline.Output;
 import org.broadinstitute.sting.commandline.RodBinding;
 import org.broadinstitute.sting.gatk.contexts.ReferenceContext;
 import org.broadinstitute.sting.gatk.refdata.RefMetaDataTracker;
 import org.broadinstitute.sting.gatk.walkers.ReadWalker;
+import org.broadinstitute.sting.gatk.walkers.TreeReducible;
 import org.broadinstitute.sting.utils.GenomeLoc;
 import org.broadinstitute.sting.utils.sam.GATKSAMRecord;
 import org.broadinstitute.sting.utils.variantcontext.VariantContext;
 
 import java.io.PrintStream;
-import java.util.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 /**
  * validate the rods for reads
  */
-public class ValidateRODForReads extends ReadWalker<Integer, Integer> {
+public class ValidateRODForReads extends ReadWalker<Integer, Integer> implements TreeReducible<Integer> {
     // a mapping of the position to the count of rods
-    HashMap<GenomeLoc, Integer> map = new LinkedHashMap<GenomeLoc, Integer>();
+    HashMap<GenomeLoc, Integer> map = new HashMap<GenomeLoc, Integer>();
 
     @Input(fullName = "variants", shortName = "V", doc="The VCF files to merge together", required=true)
     public List<RodBinding<VariantContext>> variants;
@@ -32,11 +35,13 @@ public class ValidateRODForReads extends ReadWalker<Integer, Integer> {
         if (tracker != null) {
             final List<VariantContext> features = tracker.getValues(variants);
             for ( final VariantContext f : features ) {
-                GenomeLoc location = ref.getGenomeLocParser().createGenomeLoc(f);
-                if (!map.containsKey(location)) {
-                    map.put(location,0);
+                synchronized (map) {
+                    final GenomeLoc location = ref.getGenomeLocParser().createGenomeLoc(f);
+                    if (!map.containsKey(location)) {
+                        map.put(location,0);
+                    }
+                    map.put(location,map.get(location)+1);
                 }
-                map.put(location,map.get(location)+1);
             }
 
             return features.size();
@@ -54,10 +59,15 @@ public class ValidateRODForReads extends ReadWalker<Integer, Integer> {
         return sum + value;
     }
 
+    @Override
+    public Integer treeReduce(Integer lhs, Integer rhs) {
+        return lhs + rhs;
+    }
+
     public void onTraversalDone(Integer result) {
         out.println("[REDUCE RESULT] Traversal result is: " + result + " ROD entries seen");
-        for (GenomeLoc location : map.keySet()) {
-            out.println(location + " -> " + map.get(location));
+        for (final Map.Entry<GenomeLoc, Integer> entry : new TreeMap<GenomeLoc, Integer>(map).entrySet() ) {
+            out.println(entry.getKey() + " -> " + entry.getValue());
         }
     }
 }
