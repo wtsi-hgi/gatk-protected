2e3e22f613147716c5831441963ad514b000a824
Ryan Poplin <rpoplin@broadinstitute.org>
2012-09-12 15:40:07 -0400

    adding verbose debug statements in BQSR

--- private/java/src/org/broadinstitute/sting/gatk/walkers/bqsr/DelocalizedBaseRecalibrator.java
+++ private/java/src/org/broadinstitute/sting/gatk/walkers/bqsr/DelocalizedBaseRecalibrator.java
@@ -38,6 +38,7 @@ import org.broadinstitute.sting.gatk.iterators.ReadTransformer;
 import org.broadinstitute.sting.gatk.refdata.RefMetaDataTracker;
 import org.broadinstitute.sting.gatk.walkers.*;
 import org.broadinstitute.sting.utils.BaseUtils;
+import org.broadinstitute.sting.utils.MathUtils;
 import org.broadinstitute.sting.utils.baq.BAQ;
 import org.broadinstitute.sting.utils.classloader.GATKLiteUtils;
 import org.broadinstitute.sting.utils.collections.Pair;
@@ -118,6 +119,9 @@ public class DelocalizedBaseRecalibrator extends ReadWalker<Long, Long> implemen
 
     @Argument(fullName = "bqsrBAQGapOpenPenalty", shortName="bqsrBAQGOP", doc="BQSR BAQ gap open penalty (Phred Scaled).  Default value is 40.  30 is perhaps better for whole genome call sets", required = false)
     public double BAQGOP = BAQ.DEFAULT_GOP;
+
+    @Argument(fullName="debug", shortName="debug", doc="If specified, print out very verbose debug information about each triggering active region", required = false)
+    protected boolean DEBUG;
 
     private QuantizationInfo quantizationInfo; // an object that keeps track of the information necessary for quality score quantization
 
@@ -227,13 +231,64 @@ public class DelocalizedBaseRecalibrator extends ReadWalker<Long, Long> implemen
         final int[] isDeletion = calculateIsIndel(read, EventType.BASE_DELETION);
         final byte[] baqArray = calculateBAQArray(read);
 
+        if( DEBUG ) {
+            System.out.println(read.getReadName());
+            System.out.println(read.getCigar() + " " + (read.getReadNegativeStrandFlag() ? "neg" : "pos") );
+            System.out.print("K: ");
+            for(int iii = 0; iii < skip.length; iii++) {
+                System.out.print(skip[iii] ? 1 : 0);
+            }
+            System.out.println();
+            System.out.print("S: ");
+            for(int iii = 0; iii < isSNP.length; iii++) {
+                System.out.print(isSNP[iii]);
+            }
+            System.out.println();
+            System.out.print("I: ");
+            for(int iii = 0; iii < isInsertion.length; iii++) {
+                System.out.print(isInsertion[iii]);
+            }
+            System.out.println();
+            System.out.print("D: ");
+            for(int iii = 0; iii < isDeletion.length; iii++) {
+                System.out.print(isDeletion[iii]);
+            }
+            System.out.println();
+            System.out.print("B: ");
+            for(int iii = 0; iii < baqArray.length; iii++) {
+                System.out.print((char)baqArray[iii]);
+            }
+            System.out.println();
+        }
+
         if( baqArray != null ) { // some reads just can't be BAQ'ed
             final double[] snpErrors = calculateFractionalErrorArray(isSNP, baqArray);
             final double[] insertionErrors = calculateFractionalErrorArray(isInsertion, baqArray);
             final double[] deletionErrors = calculateFractionalErrorArray(isDeletion, baqArray);
             recalibrationEngine.updateDataForRead(read, skip, snpErrors, insertionErrors, deletionErrors);
+            if( DEBUG ) {
+                System.out.print("s: ");
+                for(int iii = 0; iii < snpErrors.length; iii++) {
+                    System.out.print(MathUtils.compareDoubles(snpErrors[iii], 0.0, 1E-3)==0 ? 0 : "*");
+                }
+                System.out.println();
+                System.out.print("i: ");
+                for(int iii = 0; iii < insertionErrors.length; iii++) {
+                    System.out.print(MathUtils.compareDoubles(insertionErrors[iii], 0.0, 1E-3)==0 ? 0 : "*");
+                }
+                System.out.println();
+                System.out.print("d: ");
+                for(int iii = 0; iii < deletionErrors.length; iii++) {
+                    System.out.print(MathUtils.compareDoubles(deletionErrors[iii], 0.0, 1E-3)==0 ? 0 : "*");
+                }
+                System.out.println();
+                System.out.println("--- Data point added successfully ---");
+            }
             return 1L;
         } else {
+            if( DEBUG ) {
+                System.out.println("!!! ADD FAILED :( !!!");
+            }
             return 0L;
         }
     }
@@ -249,7 +304,7 @@ public class DelocalizedBaseRecalibrator extends ReadWalker<Long, Long> implemen
     }
 
     protected static boolean[] calculateKnownSites( final GATKSAMRecord read, final List<Feature> features ) {
-        final int BUFFER_SIZE = 10;
+        final int BUFFER_SIZE = 0;
         final int readLength = read.getReadBases().length;
         final boolean[] knownSites = new boolean[readLength];
         Arrays.fill(knownSites, false);
