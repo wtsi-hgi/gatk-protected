2201c22dd769f8450dafe28ec45f8f83cf3934d4
Mark DePristo <depristo@broadinstitute.org>
2012-10-09 10:35:07 -0400

    Intermediate commit on the path to getting a working IndependentAllelesDiploidExact calculation
    -- Still not work, but I know what's wrong
    -- Many tests disabled, that need to be reanabled

---
+++ private/R/pls.R
@@ -0,0 +1,66 @@
+pNonRef <- function(pls) {
+  totalP = 10^(pls/-10)
+  totalP = totalP / sum(totalP)
+  pNonRef = sum(totalP[2:length(pls)])
+}
+
+toBi <- function(pls, second) {
+  totalP = 10^(pls/-10)
+  totalP = totalP / sum(totalP)
+  
+  if ( second ) {
+    hom.x = c(1, 2, 3)
+    het.alt = c(4,5)
+    hom.alt = c(6)
+  } else {
+    hom.x = c(1, 4, 6)
+    het.alt = c(2,5)
+    hom.alt = c(3)
+  }
+  
+  bi = c(sum(totalP[hom.x]), sum(totalP[het.alt]), sum(totalP[hom.alt]))
+  bi = round(-10 * log10(bi))
+  bi - min(bi)
+}
+
+
+
+print(toBi(c(0, 10, 20, 30, 40, 50), F))
+print(toBi(c(0, 10, 20, 30, 40, 50), T))
+print(toBi(c(0, 10, 10, 10, 10, 10), T))
+print(toBi(c(0, 1, 2, 3, 4, 5), F))
+print(toBi(c(0, 1, 2, 3, 4, 5), T))
+
+print(toBi(c(0, 50, 50, 50, 50, 50), F))
+print(toBi(c(0, 50, 50, 50, 50, 50), T))
+
+print(toBi(c(50, 0, 50, 50, 50, 50), F))
+print(toBi(c(50, 0, 50, 50, 50, 50), T))
+
+print(toBi(c(50, 50, 0, 50, 50, 50), F))
+print(toBi(c(50, 50, 0, 50, 50, 50), T))
+
+print(toBi(c(50, 50, 50, 0, 50, 50), F))
+print(toBi(c(50, 50, 50, 0, 50, 50), T))
+
+print(toBi(c(50, 50, 50, 50, 0, 50), F))
+print(toBi(c(50, 50, 50, 50, 0, 50), T))
+
+print(toBi(c(50, 50, 50, 50, 50,  0), F))
+print(toBi(c(50, 50, 50, 50, 50,  0), T))
+
+toBi2 <- function(pls, second) {
+  totalP = 10^(pls/-10)
+  totalP = totalP / sum(totalP)
+  
+  if ( second ) {
+    het = sum(totalP[c(4,5)])
+    hom = sum(totalP[c(3,6)])
+  } else {
+    het = sum(totalP[c(2,4)])
+    hom = totalP[3]
+  }
+  
+  bi = c(totalP[1], het, hom)
+  round(-10 * log10(bi))
+}
