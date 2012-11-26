f0c91cdfb9ee9aea0083afdbfc0c1b10e588bd37
Menachem Fromer <fromer@broadinstitute.org>
2012-11-26 03:59:36 -0500

    Fixes to process all SNPs and indels simultaneously (even those at same site)

---
+++ private/java/src/org/broadinstitute/sting/gatk/walkers/genotyper/MatchFirstLocRefAltRefMetaDataTracker.java
@@ -0,0 +1,49 @@
+package org.broadinstitute.sting.gatk.walkers.genotyper;
+
+import org.broad.tribble.Feature;
+import org.broadinstitute.sting.commandline.RodBinding;
+import org.broadinstitute.sting.gatk.refdata.RefMetaDataTracker;
+import org.broadinstitute.sting.utils.GenomeLoc;
+import org.broadinstitute.sting.utils.variantcontext.VariantContext;
+
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+
+// Use this class to store a single vc to be retrieved and genotyped:
+public class MatchFirstLocRefAltRefMetaDataTracker extends RefMetaDataTracker {
+    public MatchFirstLocRefAltRefMetaDataTracker(RefMetaDataTracker tracker, VariantContext vc) {
+        super();
+        this.tracker = tracker;
+        this.vc = vc;
+    }
+
+    public <T extends Feature> List<T> getValues(final RodBinding<T> type, final GenomeLoc onlyAtThisLoc) {
+        List<T> l = new LinkedList<T>();
+
+        for (T t : tracker.getValues(type, onlyAtThisLoc)) {
+            VariantContext tVC = (VariantContext) t;
+            if (tVC.getChr().equals(vc.getChr()) && tVC.getStart() == vc.getStart() && tVC.getEnd() == vc.getEnd() && tVC.getReference().equals(vc.getReference()) && tVC.getAlternateAlleles().equals(vc.getAlternateAlleles())) {
+                l.add((T) tVC);
+                break;
+            }
+        }
+
+        return l;
+    }
+
+    public <T extends Feature> List<T> getValues(final Collection<RodBinding<T>> rodBindings, final GenomeLoc onlyAtThisLoc) {
+        List<T> l = new LinkedList<T>();
+
+        for (RodBinding<T> type : rodBindings) {
+            l.addAll(getValues(type, onlyAtThisLoc));
+            if (!l.isEmpty())
+                break;
+        }
+
+        return l;
+    }
+
+    private RefMetaDataTracker tracker;
+    private VariantContext vc;
+}
