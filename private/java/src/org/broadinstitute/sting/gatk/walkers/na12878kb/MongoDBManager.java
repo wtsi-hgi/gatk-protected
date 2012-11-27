79e147b472b6a61f801a8e676c1bba71166e8f13
Jacob Silterra <jacob@broadinstitute.org>
2012-11-27 17:44:35 -0500

    Abstract connection to MongoDB so we can specify it through JSON file. Include 2 JSON spec files in GenomeAnalysisTK.jar
    Create MongoDBManager, which keeps track of connections based on Locator class. Locators can be instantiated directly, or read from JSON files (NA12878DBArgumentCollection uses the GSon library)

---
+++ private/java/src/org/broadinstitute/sting/gatk/walkers/na12878kb/MongoDBManager.java
@@ -0,0 +1,99 @@
+package org.broadinstitute.sting.gatk.walkers.na12878kb;
+
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.Mongo;
+import org.apache.log4j.Logger;
+import org.broadinstitute.sting.utils.exceptions.StingException;
+
+import java.net.UnknownHostException;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Manages connections to Mongo databases.
+ * Connections are created and indexed with Locator instances.
+ * @author: jacob
+ * @date: Nov 27, 2012
+ */
+final public class MongoDBManager {
+    private final static Logger logger = Logger.getLogger(MongoDBManager.class);
+
+
+    private static Map<Locator, DBWrapper> connections = new HashMap<Locator, DBWrapper>(5);
+
+    public static DBWrapper getDB(Locator locator){
+        DBWrapper wrapper = connections.get(locator);
+        if(wrapper == null){
+            wrapper = new DBWrapper(locator);
+            connections.put(locator, wrapper);
+        }
+        return wrapper;
+    }
+
+    protected static void closeAll(){
+        for(DBWrapper wrapper: connections.values()){
+            wrapper.close();
+        }
+    }
+
+    public static class DBWrapper{
+        protected Mongo mongo;
+        protected DBCollection sites;
+        protected DBCollection callsets;
+        protected DBCollection consensus;
+
+        protected Mongo getMongo(){
+            return mongo;
+        }
+
+        protected DBCollection getSites() {
+            return sites;
+        }
+
+        protected DBCollection getCallsets() {
+            return callsets;
+        }
+        
+        protected DBCollection getConsensus() {
+            return consensus;
+        }
+
+        protected void close() {
+            mongo.close();
+        }
+
+        private DBWrapper(Locator locator) {
+            try {
+                logger.info("Connecting to MongoDB host=" + locator.host + " port=" + locator.port + " name=" + locator.name);
+                mongo = new Mongo(locator.host, locator.port);
+                DB mongoDB = mongo.getDB(locator.name);
+                sites = mongoDB.getCollection(locator.sitesCollection);
+                callsets = mongoDB.getCollection(locator.callsetsCollection);
+                consensus = mongoDB.getCollection(locator.consensusCollection);
+            } catch (UnknownHostException e) {
+                throw new StingException(e.getMessage(), e);
+            }
+        }
+    }
+
+    public static class Locator{
+        public final String host;
+        public final Integer port;
+        public final String name;
+        public final String sitesCollection;
+        public final String callsetsCollection;
+        public final String consensusCollection;
+
+        public Locator(String host, int port, String name, String sitesCollection,
+                       String callsetsCollection, String consensusCollection){
+            this.host = host;
+            this.port = port;
+            this.name = name;
+            this.sitesCollection = sitesCollection;
+            this.callsetsCollection = callsetsCollection;
+            this.consensusCollection = consensusCollection;
+        }
+
+    }
+}
--- private/java/src/org/broadinstitute/sting/gatk/walkers/na12878kb/NA12878DBArgumentCollection.java
+++ private/java/src/org/broadinstitute/sting/gatk/walkers/na12878kb/NA12878DBArgumentCollection.java
@@ -1,6 +1,10 @@
 package org.broadinstitute.sting.gatk.walkers.na12878kb;
 
+import com.google.gson.Gson;
 import org.broadinstitute.sting.commandline.Argument;
+import org.broadinstitute.sting.utils.exceptions.StingException;
+
+import java.io.*;
 
 /**
  * Standard arguments for interacting with the NA12878 DB
@@ -9,8 +13,21 @@ import org.broadinstitute.sting.commandline.Argument;
  * Date: 11/15/12
  */
 public class NA12878DBArgumentCollection {
+
+    public static final String DEFAULT_SPEC_PATH = "resources/NA12878kb.json";
+    public static final String LOCALHOST_SPEC_PATH = "resources/NA12878kb_local.json";
+
     @Argument(fullName = "useLocal", shortName = "useLocal", doc = "If true, the localhost MongoDB will be used; for testing only", required=false)
     protected boolean useLocal = false;
+
+    @Argument(fullName = "dbSpecPath", shortName = "dbSpecPath", doc = "Path to JSON file specifying database connection parameters", required=false)
+    protected String dbSpecPath = getDBSpecPath(useLocal);
+
+    private MongoDBManager.Locator locator;
+
+    public MongoDBManager.Locator getLocator(){
+        return locator;
+    }
 
     public enum DBType {
         /** The production database, contains the stable data for analysis */
@@ -39,9 +56,54 @@ public class NA12878DBArgumentCollection {
     @Argument(shortName = "reset", required=false)
     protected boolean resetDB = false;
 
-    public NA12878DBArgumentCollection(){}
+    /**
+     * Calls {@link org.broadinstitute.sting.gatk.walkers.na12878kb.NA12878DBArgumentCollection#NA12878DBArgumentCollection(false)}
+     */
+    public NA12878DBArgumentCollection(){
+        this(false);
+    }
 
+    private static String getDBSpecPath(boolean useLocal){
+        return useLocal ? LOCALHOST_SPEC_PATH : DEFAULT_SPEC_PATH;
+    }
+
+    /**
+     * Convenience constructor for choosing between local or remote
+     * NA12878KB database
+     * @param useLocal
+     */
     public NA12878DBArgumentCollection(boolean useLocal){
         this.useLocal = useLocal;
+        this.dbSpecPath = getDBSpecPath(useLocal);
+        initLocator(dbSpecPath);
     }
+
+    /**
+     * Uses the provided file to load database host/name/port/etc.
+     * Must be appropriate JSON format
+     * @param dbSpecPath
+     */
+    public NA12878DBArgumentCollection(String dbSpecPath){
+        this.dbSpecPath = dbSpecPath;
+        initLocator(dbSpecPath);
+
+    }
+
+    private void initLocator(String dbSpecPath){
+        InputStream is = getClass().getResourceAsStream(dbSpecPath);
+        if(is == null){
+            try {
+                is = new FileInputStream(dbSpecPath);
+            } catch (FileNotFoundException e) {
+                throw new StingException("db spec path not found", e);
+            }
+        }
+
+        Reader reader = new InputStreamReader(is);
+        MongoDBManager.Locator tmpLocator = (new Gson()).fromJson(reader, MongoDBManager.Locator.class);
+        String dbName = tmpLocator.name + dbToUse.getExtension();
+        this.locator = new MongoDBManager.Locator(tmpLocator.host, tmpLocator.port, dbName, tmpLocator.sitesCollection,
+                tmpLocator.callsetsCollection, tmpLocator.consensusCollection);
+    }
+
 }
