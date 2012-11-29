8ebfac34cf566aefebbb4730c8b7bffaaad95942
Jacob Silterra <jacob@broadinstitute.org>
2012-11-29 11:03:21 -0500

    Fix NA12878DBArgumentCollectionUnitTest so it uses testng, and testCompareLocalRemoteLocators compare the right things

--- private/java/test/org/broadinstitute/sting/gatk/walkers/na12878kb/NA12878DBArgumentCollectionUnitTest.java
+++ private/java/test/org/broadinstitute/sting/gatk/walkers/na12878kb/NA12878DBArgumentCollectionUnitTest.java
@@ -1,7 +1,7 @@
 package org.broadinstitute.sting.gatk.walkers.na12878kb;
 
-import junit.framework.Assert;
 import org.broadinstitute.sting.BaseTest;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 /**
@@ -28,13 +28,12 @@ public class NA12878DBArgumentCollectionUnitTest extends BaseTest {
         NA12878DBArgumentCollection args1 = new NA12878DBArgumentCollection(false);
         MongoDBManager.Locator remoteLocator = args1.getLocator();
 
-
         Assert.assertNotSame(localLocator, remoteLocator);
         Assert.assertNotSame(localLocator.host, remoteLocator.host);
         Assert.assertEquals(localLocator.name, remoteLocator.name);
         Assert.assertEquals(localLocator.port, remoteLocator.port);
         Assert.assertEquals(localLocator.callsetsCollection, remoteLocator.callsetsCollection);
         Assert.assertEquals(localLocator.consensusCollection, remoteLocator.consensusCollection);
-        Assert.assertEquals(localLocator.callsetsCollection, remoteLocator.sitesCollection);
+        Assert.assertEquals(localLocator.sitesCollection, remoteLocator.sitesCollection);
     }
 }
