088dc3db139b00d54a466d8d6bcb786f398932b8
Mauricio Carneiro <carneiro@broadinstitute.org>
2012-12-05 01:06:19 -0500

    Added bootstrapping and fixed the GLM model of the FMCC

--- private/java/src/org/broadinstitute/sting/gatk/walkers/qc/FindMinimumCallableCoverage.java
+++ private/java/src/org/broadinstitute/sting/gatk/walkers/qc/FindMinimumCallableCoverage.java
@@ -1,13 +1,12 @@
 package org.broadinstitute.sting.gatk.walkers.qc;
 
-import org.broadinstitute.sting.commandline.Input;
-import org.broadinstitute.sting.commandline.Output;
-import org.broadinstitute.sting.commandline.RodBinding;
+import org.broadinstitute.sting.commandline.*;
 import org.broadinstitute.sting.gatk.contexts.AlignmentContext;
 import org.broadinstitute.sting.gatk.contexts.ReferenceContext;
 import org.broadinstitute.sting.gatk.refdata.RefMetaDataTracker;
 import org.broadinstitute.sting.gatk.report.GATKReport;
 import org.broadinstitute.sting.gatk.walkers.*;
+import org.broadinstitute.sting.gatk.walkers.genotyper.GenotypeLikelihoodsCalculationModel;
 import org.broadinstitute.sting.gatk.walkers.genotyper.UnifiedArgumentCollection;
 import org.broadinstitute.sting.gatk.walkers.genotyper.UnifiedGenotyperEngine;
 import org.broadinstitute.sting.gatk.walkers.genotyper.VariantCallContext;
@@ -40,18 +39,30 @@ public class FindMinimumCallableCoverage extends RodWalker<Integer, Integer> {
     @Input(fullName="alleles", shortName = "alleles", doc="The set of alleles at which to genotype", required=true)
     public RodBinding<VariantContext> alleles;
 
+    @Argument(fullName = "bootstrap", shortName = "boot", doc = "Number of bootstrap interations", required = false)
+    public int bootstrapIterations = 100;
+
+    @Hidden
+    @Argument(fullName = "debugLevel", shortName = "dl", doc = "output debug information", required = false)
+    public int debugLevel = 0;
 
     private GATKReport report;
-    private UnifiedGenotyperEngine genotyperEngine;
+    private UnifiedGenotyperEngine snpEngine;
+    private UnifiedGenotyperEngine indelEngine;
     private double callConf = 0.0;
+    private long mapCounter = 1;
 
     @Override
     public void initialize() {
         super.initialize();
-        final UnifiedArgumentCollection uac = new UnifiedArgumentCollection();
-        genotyperEngine = new UnifiedGenotyperEngine(getToolkit(), uac);
-        callConf = uac.STANDARD_CONFIDENCE_FOR_CALLING;
-        report = GATKReport.newSimpleReport("Position", "MinimumCallableCoverage", "EventComplexity", "VariantType", "GenotypeType");
+        final UnifiedArgumentCollection snpUAC = new UnifiedArgumentCollection();
+        final UnifiedArgumentCollection indelUAC = new UnifiedArgumentCollection();
+        snpUAC.GLmodel = GenotypeLikelihoodsCalculationModel.Model.SNP;
+        indelUAC.GLmodel = GenotypeLikelihoodsCalculationModel.Model.INDEL;
+        snpEngine = new UnifiedGenotyperEngine(getToolkit(), snpUAC);
+        indelEngine = new UnifiedGenotyperEngine(getToolkit(), indelUAC);
+        callConf = snpUAC.STANDARD_CONFIDENCE_FOR_CALLING;
+        report = GATKReport.newSimpleReport("MinCov", "Position", "MinimumCallableCoverage", "EventComplexity", "VariantType", "GenotypeType");
     }
 
     @Override
@@ -69,24 +80,39 @@ public class FindMinimumCallableCoverage extends RodWalker<Integer, Integer> {
         eventInfo.variantType = vcComp.isSNP() ? VariantContext.Type.SNP : VariantContext.Type.INDEL;
         eventInfo.genotypeType = vcComp.getGenotype(0).getType();   // assumes single sample analysis
 
-        int coverage = context.getBasePileup().depthOfCoverage();
         boolean wasCalledOnce = false;
-        while (coverage > 0) {
-            List<VariantCallContext> callList = genotyperEngine.calculateLikelihoodsAndGenotypes(tracker, ref, context);
-            VariantCallContext call = callList.get(0) == null && callList.size() > 1 ? callList.get(1) : callList.get(0);    // if we have more than one calls and the first one is false (snp) get the second.
+        int sum = 0;
 
-            if (call != null && call.isCalledAlt(callConf)) {
-                context.downsampleToCoverage(--coverage);
-                wasCalledOnce = true;
-            }
-            else {
-                if (wasCalledOnce) {
-                    eventInfo.coverageNeededToCall = coverage + 1;
-                    eventInfo.emit();
+        if (debugLevel > 0) System.out.print(mapCounter++ + ": ");
+
+        for (int i=0; i<bootstrapIterations; i++) {
+            int coverage = context.getBasePileup().depthOfCoverage();
+            final AlignmentContext copyOfContext = new AlignmentContext(context.getLocation(), context.getBasePileup().copy(), context.getSkippedBases(), context.hasPileupBeenDownsampled());
+            while (coverage > 0) {
+                final List<VariantCallContext> callList = vcComp.isSNP() ?
+                        snpEngine.calculateLikelihoodsAndGenotypes(tracker, ref, copyOfContext) :
+                        indelEngine.calculateLikelihoodsAndGenotypes(tracker, ref, copyOfContext);
+                final VariantCallContext call = callList.isEmpty() ? null : callList.get(0);
+                if (call != null && call.isCalledAlt(callConf) && call.getType() == eventInfo.variantType) {
+                    copyOfContext.downsampleToCoverage(--coverage);
+                    wasCalledOnce = true;
                 }
-                break;
+                else {
+                    if (wasCalledOnce) {
+                        sum += coverage+1;
+                        if (debugLevel > 0) System.out.print(coverage+1 + ", ");
+                    }
+                    break;
+                }
             }
         }
+        if (wasCalledOnce) {
+            eventInfo.coverageNeededToCall = sum/bootstrapIterations;
+            eventInfo.emit();
+            if (debugLevel > 0) System.out.print("[" + eventInfo.coverageNeededToCall + "]");
+        }
+        if (debugLevel > 0) System.out.println();
+
 
         return wasCalledOnce ? 0 : 1;
     }
@@ -96,7 +122,7 @@ public class FindMinimumCallableCoverage extends RodWalker<Integer, Integer> {
         return 0;  //To change body of implemented methods use File | Settings | File Templates.
     }
 
-    @java.lang.Override
+    @Override
     public Integer reduce(Integer value, Integer sum) {
         return sum+value;  //To change body of implemented methods use File | Settings | File Templates.
     }
