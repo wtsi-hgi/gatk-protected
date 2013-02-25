bd600076cb4c6106be14d840b2cfba291c919632
David Roazen <droazen@broadinstitute.org>
2013-02-25 10:17:16 -0500

    Update release scripts in preparation for open-sourcing protected

--- private/gsa-engineering/release_exporter/filter_stable.sh
+++ private/gsa-engineering/release_exporter/filter_stable.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #
-# Removes private/protected files and directories from the git history of the clone it's run in. Must only be 
+# Removes blacklisted files and directories from the git history of the clone it's run in. Must only be 
 # run by the gsa-engineering user, and only on a clone of Stable.
 #
 # Author: David Roazen
 #
 
-# Directories to filter out of the git history for the public release. IMPORTANT: If we ever change this list
+# Directories to filter out of the git history for the specified release. IMPORTANT: If we ever change these lists
 # (EXCEPT to blacklist a brand-new directory that didn't previously exist in the repository) we'll need to 
 # rebuild the github repository from scratch, and everyone who has cloned it will need to delete their old 
 # clones and re-clone it. This is due to the way history re-writing in git works: the commit ids all change 
@@ -14,13 +14,34 @@
 # you change the directories to be deleted you end up creating an entirely new git history unrelated to the 
 # former history, making it impossible to update the repository through a simple "git push" and breaking
 # synchronization for everyone. In other words, THINK CAREFULLY BEFORE CHANGING THIS VARIABLE!
-BLACKLIST="private protected"
+PUBLIC_REPO_BLACKLIST="private protected"
+PROTECTED_REPO_BLACKLIST="private"
 
-GITHUB_URL="git@github.com:broadgsa/gatk.git"
+GATK_PUBLIC_REPO_URL="git@github.com:broadgsa/gatk.git"
+GATK_PROTECTED_REPO_URL="git@github.com:broadgsa/gatk-protected.git"
+
+if [ $# -ne 1 ]
+then
+    echo "Usage: $0 release_target"
+    exit 1
+fi
+
+if [ "$1" == "public" ]
+then
+    BLACKLIST="${PUBLIC_REPO_BLACKLIST}"
+    GITHUB_URL="${GATK_PUBLIC_REPO_URL}"
+elif [ "$1" == "protected" ]
+then
+    BLACKLIST="${PROTECTED_REPO_BLACKLIST}"
+    GITHUB_URL="${GATK_PROTECTED_REPO_URL}"
+else
+    echo "$0: invalid release target $1"
+    exit 1
+fi
 
 echo "$0: Prior to filtering, local HEAD = `git rev-parse HEAD`"
 
-echo "$0: Filtering history in clone `pwd` to remove private directories"
+echo "$0: Filtering history in clone `pwd` to remove blacklisted directories"
 git filter-branch --index-filter "git rm -rf --cached --ignore-unmatch ${BLACKLIST}" --tag-name-filter cat | grep -v '^rm ' 
 
 if [ $? -ne 0 ]
--- private/gsa-engineering/release_exporter/source_release.sh
+++ private/gsa-engineering/release_exporter/source_release.sh
@@ -1,15 +1,33 @@
 #!/bin/bash
 
-RELEASE_URL="git@github.com:broadgsa/gatk.git"
+GATK_PUBLIC_REPO_URL="git@github.com:broadgsa/gatk.git"
+GATK_PROTECTED_REPO_URL="git@github.com:broadgsa/gatk-protected.git"
 
-git remote add github "${RELEASE_URL}"
+if [ $# -ne 1 ]
+then
+    echo "Usage: $0 release_target"
+    exit 1
+fi
+
+if [ "$1" == "public" ]
+then
+    TARGET_REPO="${GATK_PUBLIC_REPO_URL}"
+elif [ "$1" == "protected" ]
+then
+    TARGET_REPO="${GATK_PROTECTED_REPO_URL}"
+else
+    echo "$0: Invalid release target: $1"
+    exit 1
+fi
+
+git remote add github "${TARGET_REPO}"
 
 NEW_HEAD=`git rev-parse HEAD`
 REMOTE_HEAD=`git ls-remote --heads github | grep refs/heads/master | awk '{print $1}'`
 
 if [ "${NEW_HEAD}" == "${REMOTE_HEAD}" ]
 then
-    echo "$0: Release repository up-to-date with respect to Stable, no source release performed (HEAD = ${REMOTE_HEAD})"
+    echo "$0: Release repository ${TARGET_REPO} up-to-date with respect to Stable, no source release performed (HEAD = ${REMOTE_HEAD})"
     exit 0
 fi
 
