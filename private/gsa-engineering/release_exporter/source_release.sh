42b714ff14d888513221f2532dd2495b0fdc75cb
David Roazen <droazen@broadinstitute.org>
2013-02-26 14:17:39 -0500

    Temporarily disable paranoid validation in the release scripts
    These validation steps are not strictly necessary, and would fail
    with the protected repo right now, as it currently lacks a master
    branch.

--- private/gsa-engineering/release_exporter/source_release.sh
+++ private/gsa-engineering/release_exporter/source_release.sh
@@ -22,14 +22,16 @@ fi
 
 git remote add github "${TARGET_REPO}"
 
-NEW_HEAD=`git rev-parse HEAD`
-REMOTE_HEAD=`git ls-remote --heads github | grep refs/heads/master | awk '{print $1}'`
-
-if [ "${NEW_HEAD}" == "${REMOTE_HEAD}" ]
-then
-    echo "$0: Release repository ${TARGET_REPO} up-to-date with respect to Stable, no source release performed (HEAD = ${REMOTE_HEAD})"
-    exit 0
-fi
+# Disable the check against the remote HEAD for now, as it's not strictly necessary.
+#
+# NEW_HEAD=`git rev-parse HEAD`
+# REMOTE_HEAD=`git ls-remote --heads github | grep refs/heads/master | awk '{print $1}'`
+#
+# if [ "${NEW_HEAD}" == "${REMOTE_HEAD}" ]
+# then
+#     echo "$0: Release repository ${TARGET_REPO} up-to-date with respect to Stable, no source release performed (HEAD = ${REMOTE_HEAD})"
+#     exit 0
+# fi
 
 git push --tags github master:master 
 
