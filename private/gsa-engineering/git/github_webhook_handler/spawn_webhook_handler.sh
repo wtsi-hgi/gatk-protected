e28f175c8c27f6b37c7d323ebdb13bc4db8b85f0
David Roazen <droazen@broadinstitute.org>
2013-03-19 03:45:24 -0400

    Revert "github webhook handler: convert from daemon to cron job"
    Turns out the email script doesn't work correctly from cron.
    Converting the webhook script back to a daemon for now until
    it can be made to work as a cron job.
    
    This reverts commit 9679accb641537f5c637cce0aeb63f3925521b42.

---
+++ private/gsa-engineering/git/github_webhook_handler/spawn_webhook_handler.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# 
+# Respawn the github webhook handler if necessary. 
+#
+# Author: David Roazen
+#
+
+HANDLER="/humgen/gsa-scr1/gsa-engineering/git/github_webhook_handler/handle_webhooks.sh"
+HANDLER_NAME="handle_webhooks"
+HANDLER_USER="gsa-engineering"
+
+if ! ps -U "${HANDLER_USER}" -o comm | grep "${HANDLER_NAME}"
+then
+    echo "Handler not found, restarting ${HANDLER}"
+    nohup "${HANDLER}" > /dev/null &
+    sleep 5 
+fi
+
+exit 0
+
