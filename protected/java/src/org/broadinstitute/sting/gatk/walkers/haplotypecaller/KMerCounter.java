c9c6faebd11821d36eaa1821c313c738e3dff9b5
Mark DePristo <depristo@broadinstitute.org>
2013-03-30 14:22:45 -0400

    Major improvements to HC that trims down active regions before genotyping
    -- Trims down active regions and associated reads and haplotypes to a smaller interval based on the events actually in the haplotypes within the original active region (without extension).  Radically speeds up calculations when using large active region extensions.  The ActiveRegion.trim algorithm does the best job it can of trimming an active region down to a requested interval while ensuring the resulting active region has a region (and extension) no bigger than the original while spanning as much of the requested extend as possible.  The trimming results in an active region that is a subset of the previous active region based on the position and types of variants found among the haplotypes
    -- Retire error corrector, archive old code and repurpose subsystem into a general kmer counter.  The previous error corrector was just broken (conceptually) and was disabled by default in the engine.  Now turning on error correction throws a UserException. Old part of the error corrector that counts kmers was extracted and put into KMerCounter.java
    -- Add final simplify graph call after we prune away the non-reference paths in DeBruijnAssembler

--- protected/java/src/org/broadinstitute/sting/gatk/walkers/haplotypecaller/KMerCounter.java
+++ protected/java/src/org/broadinstitute/sting/gatk/walkers/haplotypecaller/KMerCounter.java
@@ -44,115 +44,110 @@
 *  7.7 Governing Law. This Agreement shall be construed, governed, interpreted and applied in accordance with the internal laws of the Commonwealth of Massachusetts, U.S.A., without regard to conflict of laws principles.
 */
 
-package org.broadinstitute.sting.gatk.walkers.haplotypecaller.graphs;
+package org.broadinstitute.sting.gatk.walkers.haplotypecaller;
 
-import com.google.java.contract.Ensures;
+import org.apache.log4j.Logger;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 /**
- * A DeBruijn kmer graph
+ * generic utility class that counts kmers
  *
- * User: rpoplin
- * Date: 2/6/13
+ * Basically you add kmers to the counter, and it tells you how many occurrences of each kmer it's seen.
+ *
+ * User: depristo
+ * Date: 3/8/13
+ * Time: 1:16 PM
  */
-public final class DeBruijnGraph extends BaseGraph<DeBruijnVertex> {
+public class KMerCounter {
+    private final static Logger logger = Logger.getLogger(KMerCounter.class);
+
     /**
-     * Create an empty DeBruijnGraph with default kmer size
+     * A map of for each kmer to its num occurrences in addKmers
      */
-    public DeBruijnGraph() {
-        super();
+    private final Map<String, CountedKmer> countsByKMer = new HashMap<String, CountedKmer>();
+    private final int kmerLength;
+
+    /**
+     * Create a new kmer counter
+     *
+     * @param kmerLength the length of kmers we'll be counting to error correct, must be >= 1
+     */
+    public KMerCounter(final int kmerLength) {
+        if ( kmerLength < 1 ) throw new IllegalArgumentException("kmerLength must be > 0 but got " + kmerLength);
+        this.kmerLength = kmerLength;
     }
 
     /**
-     * Create an empty DeBruijnGraph with kmer size
-     * @param kmerSize kmer size, must be >= 1
+     * For testing purposes
+     *
+     * @param kmers
      */
-    public DeBruijnGraph(int kmerSize) {
-        super(kmerSize);
+    protected void addKmers(final String ... kmers) {
+        for ( final String kmer : kmers )
+            addKmer(kmer, 1);
     }
 
     /**
-     * Pull kmers out of the given long sequence and throw them on in the graph
-     * @param sequence      byte array holding the sequence with which to build the assembly graph
-     * @param KMER_LENGTH   the desired kmer length to use
-     * @param isRef         if true the kmers added to the graph will have reference edges linking them
+     * Get the count of kmer in this kmer counter
+     * @param kmer a non-null counter to get
+     * @return a positive integer
      */
-    public void addSequenceToGraph( final byte[] sequence, final int KMER_LENGTH, final boolean isRef ) {
-        if( sequence.length < KMER_LENGTH + 1 ) { throw new IllegalArgumentException("Provided sequence is too small for the given kmer length"); }
-        final int kmersInSequence = sequence.length - KMER_LENGTH + 1;
-        for( int iii = 0; iii < kmersInSequence - 1; iii++ ) {
-            addKmersToGraph(Arrays.copyOfRange(sequence, iii, iii + KMER_LENGTH), Arrays.copyOfRange(sequence, iii + 1, iii + 1 + KMER_LENGTH), isRef, 1);
+    public int getKmerCount(final byte[] kmer) {
+        if ( kmer == null ) throw new IllegalArgumentException("kmer cannot be null");
+        final CountedKmer counted = countsByKMer.get(new String(kmer));
+        return counted == null ? 0 : counted.count;
+    }
+
+    /**
+     * Add a kmer that occurred kmerCount times
+     *
+     * @param rawKmer a kmer
+     * @param kmerCount the number of occurrences
+     */
+    public void addKmer(final byte[] rawKmer, final int kmerCount) {
+        addKmer(new String(rawKmer), kmerCount);
+    }
+
+    protected void addKmer(final String rawKmer, final int kmerCount) {
+        if ( rawKmer.length() != kmerLength ) throw new IllegalArgumentException("bad kmer length " + rawKmer + " expected size " + kmerLength);
+        if ( kmerCount < 0 ) throw new IllegalArgumentException("bad kmerCount " + kmerCount);
+
+        CountedKmer countFromMap = countsByKMer.get(rawKmer);
+        if ( countFromMap == null ) {
+            countFromMap = new CountedKmer(rawKmer);
+            countsByKMer.put(rawKmer, countFromMap);
         }
+        countFromMap.count += kmerCount;
     }
 
-    /**
-     * Add edge to assembly graph connecting the two kmers
-     * @param kmer1 the source kmer for the edge
-     * @param kmer2 the target kmer for the edge
-     * @param isRef true if the added edge is a reference edge
-     */
-    public void addKmersToGraph( final byte[] kmer1, final byte[] kmer2, final boolean isRef, final int multiplicity ) {
-        if( kmer1 == null ) { throw new IllegalArgumentException("Attempting to add a null kmer to the graph."); }
-        if( kmer2 == null ) { throw new IllegalArgumentException("Attempting to add a null kmer to the graph."); }
-        if( kmer1.length != kmer2.length ) { throw new IllegalArgumentException("Attempting to add a kmers to the graph with different lengths."); }
-
-        final DeBruijnVertex v1 = new DeBruijnVertex( kmer1 );
-        final DeBruijnVertex v2 = new DeBruijnVertex( kmer2 );
-        final BaseEdge toAdd = new BaseEdge(isRef, multiplicity);
-
-        addVertices(v1, v2);
-        addOrUpdateEdge(v1, v2, toAdd);
+    @Override
+    public String toString() {
+        final StringBuilder b = new StringBuilder("KMerCounter{");
+        b.append("counting ").append(countsByKMer.size()).append(" distinct kmers");
+        b.append("\n}");
+        return b.toString();
     }
 
-    /**
-     * Higher-level interface to #addKmersToGraph that adds a pair of kmers from a larger sequence of bytes to this
-     * graph.  The kmers start at start (first) and start + 1 (second) have have length getKmerSize().  The
-     * edge between them is added with isRef and multiplicity
-     *
-     * @param sequence a sequence of bases from which we want to extract a pair of kmers
-     * @param start the start of the first kmer in sequence, must be between 0 and sequence.length - 2 - getKmerSize()
-     * @param isRef should the edge between the two kmers be a reference edge?
-     * @param multiplicity what's the multiplicity of the edge between these two kmers
-     */
-    public void addKmerPairFromSeqToGraph( final byte[] sequence, final int start, final boolean isRef, final int multiplicity ) {
-        if ( sequence == null ) throw new IllegalArgumentException("Sequence cannot be null");
-        if ( start < 0 ) throw new IllegalArgumentException("start must be >= 0 but got " + start);
-        if ( start + 1 + getKmerSize() > sequence.length ) throw new IllegalArgumentException("start " + start + " is too big given kmerSize " + getKmerSize() + " and sequence length " + sequence.length);
-        final byte[] kmer1 = Arrays.copyOfRange(sequence, start, start + getKmerSize());
-        final byte[] kmer2 = Arrays.copyOfRange(sequence, start + 1, start + 1 + getKmerSize());
-        addKmersToGraph(kmer1, kmer2, isRef, multiplicity);
-    }
+    private static class CountedKmer implements Comparable<CountedKmer> {
+        final String kmer;
+        int count;
 
-    /**
-     * Convert this kmer graph to a simple sequence graph.
-     *
-     * Each kmer suffix shows up as a distinct SeqVertex, attached in the same structure as in the kmer
-     * graph.  Nodes that are sources are mapped to SeqVertex nodes that contain all of their sequence
-     *
-     * @return a newly allocated SequenceGraph
-     */
-    @Ensures({"result != null"})
-    public SeqGraph convertToSequenceGraph() {
-        final SeqGraph seqGraph = new SeqGraph(getKmerSize());
-        final Map<DeBruijnVertex, SeqVertex> vertexMap = new HashMap<DeBruijnVertex, SeqVertex>();
-
-        // create all of the equivalent seq graph vertices
-        for ( final DeBruijnVertex dv : vertexSet() ) {
-            final SeqVertex sv = new SeqVertex(dv.getAdditionalSequence(isSource(dv)));
-            vertexMap.put(dv, sv);
-            seqGraph.addVertex(sv);
+        private CountedKmer(String kmer) {
+            this.kmer = kmer;
         }
 
-        // walk through the nodes and connect them to their equivalent seq vertices
-        for( final BaseEdge e : edgeSet() ) {
-            final SeqVertex seqOutV = vertexMap.get(getEdgeTarget(e));
-            final SeqVertex seqInV = vertexMap.get(getEdgeSource(e));
-            seqGraph.addEdge(seqInV, seqOutV, e);
+        @Override
+        public String toString() {
+            return "CountedKmer{" +
+                    "kmer='" + kmer + '\'' +
+                    ", count=" + count +
+                    '}';
         }
 
-        return seqGraph;
+        @Override
+        public int compareTo(CountedKmer o) {
+            return o.count - count;
+        }
     }
 }
