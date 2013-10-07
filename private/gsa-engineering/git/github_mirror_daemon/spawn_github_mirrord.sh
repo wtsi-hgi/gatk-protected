dc6259138058d45deafc91b7eb3d317d3097666b
David Roazen <droazen@broadinstitute.org>
2013-10-07 14:35:59 -0400

    Make cron daemon-spawning scripts less chatty
    Cuts down on gsa-engineering cron emails

--- private/gsa-engineering/git/github_mirror_daemon/spawn_github_mirrord.sh
+++ private/gsa-engineering/git/github_mirror_daemon/spawn_github_mirrord.sh
@@ -9,13 +9,10 @@ DAEMON="/local/gsa-engineering/github_mirrors/github_mirrord.sh"
 DAEMON_NAME="github_mirrord"
 DAEMON_USER="gsa-engineering"
 
-if ! ps -U "${DAEMON_USER}" -o comm | grep "${DAEMON_NAME}"
+if ! ps -U "${DAEMON_USER}" -o comm | grep -q "${DAEMON_NAME}"
 then
-    echo "Github mirror daemon not found, restarting ${DAEMON}"
     nohup "${DAEMON}" > /dev/null &
     sleep 5 
-else
-    echo "Github mirror daemon running, no action required"
 fi
 
 exit 0
