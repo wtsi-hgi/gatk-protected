1006061dd00e4f704d41bf4c492f06224108bc54
David Roazen <droazen@broadinstitute.org>
2014-06-06 15:19:28 -0400

    Stop downloading GATK reports from legacy AWS account
    Our old AWS account is due to be deactivated today. Starting tonight,
    we'll no longer attempt to download GATK reports from it.

--- private/GATKLogs/downloadGATKReportsFromS3.sh
+++ private/GATKLogs/downloadGATKReportsFromS3.sh
@@ -15,7 +15,8 @@ global_timeout_in_seconds=80000
 s3funnel_dir="${download_root}/s3funnel_mod"
 s3funnel_threads=10
 gsa_bucket="broad.gsa.gatk.run.reports"
-mark_bucket="GATK_Run_Reports"
+account="gsa"
+s3_bucket="${gsa_bucket}"
 
 export PYTHONPATH="${s3funnel_dir}:${PYTHONPATH}"
 
@@ -25,44 +26,34 @@ log_event() {
 
 total_chunks=0
 
-for account in gsa mark
+s3_config_file="${s3_config_dir}/s3cfg_${account}"
+download_master_id="${timestamp}_${account}"
+bucket_list_file="${download_root}/${download_master_id}_ls"
+
+export AWS_ACCESS_KEY_ID=`grep access_key "${s3_config_file}" | awk -F' = ' '{ print $2; }'`
+export AWS_SECRET_ACCESS_KEY=`grep secret_key "${s3_config_file}" | awk -F' = ' '{ print $2; }'`
+
+log_event "Retrieving file listing for account ${account}"
+"${s3funnel_dir}/s3funnel" --insecure -t "${s3funnel_threads}" "${s3_bucket}" LIST > "${bucket_list_file}"
+log_event "Done retrieving file listing for account ${account}"
+
+cd "${download_root}"
+rm -f *.done
+chunk_prefix="${download_master_id}_part_"
+split -d -a 3 -l "${download_chunk_size}" "${bucket_list_file}" "${chunk_prefix}"
+
+num_chunks=`ls -1 ${chunk_prefix}* | wc -l`
+total_chunks=`expr ${total_chunks} + ${num_chunks}`
+log_event "Processing ${num_chunks} chunks for account ${account}"
+
+for chunk in `ls -1 ${chunk_prefix}*`
 do
-    if [ $account == "gsa" ]
-    then
-        s3_bucket="${gsa_bucket}"
-    else
-        s3_bucket="${mark_bucket}"
-    fi
-
-    s3_config_file="${s3_config_dir}/s3cfg_${account}"
-    download_master_id="${timestamp}_${account}"
-    bucket_list_file="${download_root}/${download_master_id}_ls"
-
-    export AWS_ACCESS_KEY_ID=`grep access_key "${s3_config_file}" | awk -F' = ' '{ print $2; }'`
-    export AWS_SECRET_ACCESS_KEY=`grep secret_key "${s3_config_file}" | awk -F' = ' '{ print $2; }'`
-
-    log_event "Retrieving file listing for account ${account}"
-    "${s3funnel_dir}/s3funnel" --insecure -t "${s3funnel_threads}" "${s3_bucket}" LIST > "${bucket_list_file}"
-    log_event "Done retrieving file listing for account ${account}"
-
-    cd "${download_root}"
-    rm -f *.done
-    chunk_prefix="${download_master_id}_part_"
-    split -d -a 3 -l "${download_chunk_size}" "${bucket_list_file}" "${chunk_prefix}"
-
-    num_chunks=`ls -1 ${chunk_prefix}* | wc -l`
-    total_chunks=`expr ${total_chunks} + ${num_chunks}`
-    log_event "Processing ${num_chunks} chunks for account ${account}"
-
-    for chunk in `ls -1 ${chunk_prefix}*`
-    do
-        log_event "Dispatching asynchronous downloader process for chunk ${chunk}"
-        nohup "${script_dir}/downloadGATKReportsChunk.sh" "${chunk}" "${s3_bucket}" "${s3_config_file}" > "${chunk}.log" 2>&1 &
-        sleep 1
-    done
-
-    rm "${bucket_list_file}"
+    log_event "Dispatching asynchronous downloader process for chunk ${chunk}"
+    nohup "${script_dir}/downloadGATKReportsChunk.sh" "${chunk}" "${s3_bucket}" "${s3_config_file}" > "${chunk}.log" 2>&1 &
+    sleep 1
 done
+
+rm "${bucket_list_file}"
 
 num_done_chunks=0
 elapsed_seconds=0
